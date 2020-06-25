ea21dd989943a3a4127d916519cf14e4ed65df73
Aleksa Krolls <aleksagk@gmail.com>
2020-06-25 16:56:30 -0500

    aleksa@openfn.org's change on OpenFn.org

--- connTest.js
+++ connTest.js
@@ -1,9 +1,10 @@
  sql({
   query: `
     SELECT NAME
-    FROM sys.objects where type='u' and name like 'wcsprograms_kobo%'
+    FROM sys.objects where type='u' and name like 'WCSPROGRAMS_KoboNgrt%'
    `,
   });
+  
 
 sql({
   query: `
