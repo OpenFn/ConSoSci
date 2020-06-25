0208141718f49e16bf71dfddd6c2c852b739a998
aleksa-krolls <aleksagk@gmail.com>
2020-06-25 15:51:45 -0500

    add id

--- bns/price.js
+++ bns/price.js
@@ -1,5 +1,6 @@
 upsert('WCSPROGRAMS_KoboBnsPrice', 'DatasetUuidId', {
   DatasetUuidId: dataValue('_uuid'),
+  Id: dataValue('_id'),
   Surveyor: dataValue('body.surveyor'),
   Village: dataValue('body.village'),
 });
