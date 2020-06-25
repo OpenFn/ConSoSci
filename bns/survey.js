fb318d9dd2392bce31f69c87ae14843b3e592305
Taylor Downs <downs.taylor@gmail.com>
2020-06-25 10:31:53 -0600

    expand mapping example for survey.js

--- bns/survey.js
+++ bns/survey.js
@@ -1,12 +1,14 @@
 // Built for Microsoft SQL Azure (RTM) - 12.0.2000.8
 upsert('WCSPROGRAMS_KoboBnsAnswer', 'DatasetUuidId', {
   DatasetUuidId: dataValue('_uuid'),
+  // other: dataValue('otherStuff'),
+  // more: dataValue('moreFields'),
 });
 
 // Refactor this for scale so it doesn't perform a no-op delete 9/10 times.
 // Maybe check result of previous op, then only delete if it was an update.
 sql({
-  query: `DELETE FROM WCSPROGRAMS_KoboBnsAnswerhhmembers where AnswerId = ${state.data.AnswerId}`
+  query: `DELETE FROM WCSPROGRAMS_KoboBnsAnswerhhmembers where AnswerId = ${state.data.AnswerId}`,
 });
 
 insertMany('WCSPROGRAMS_KoboBnsAnswerhhmembers', state => {
