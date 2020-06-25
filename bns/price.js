0056b42ce5dd4d9a4791d56ae914313961044503
aleksa-krolls <aleksagk@gmail.com>
2020-06-25 16:07:59 -0500

    updating comments

--- bns/price.js
+++ bns/price.js
@@ -5,11 +5,11 @@ upsert('WCSPROGRAMS_KoboBnsPrice', 'DatasetUuidId', {
   Village: state.data.body.village,
   Gs: state.data.body.good[0][`good/name`], //repeat group
   Price: state.data.body.good[0][`good/price`], //repeat group
-  LastUpdate: state.data.body._submission_time //Cannot insert the value NULL into column 'LastUpdate'
+  LastUpdate: state.data.body._submission_time //Cannot insert the value NULL into column 'LastUpdate'--> use submission time? 
 });
 
 
-//Need a for each option
+//Need a `for each` option in LP. Something like...
 /*each(
   merge(
     dataPath('body.good[*]'),
