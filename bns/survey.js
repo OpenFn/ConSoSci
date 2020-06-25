6b048060d974e7ce3c52ebaac3ac64b5b24122cf
Taylor Downs <downs.taylor@gmail.com>
2020-06-25 10:30:39 -0600

    new format for survey.js

--- bns/survey.js
+++ bns/survey.js
@@ -1,55 +1,19 @@
 // Built for Microsoft SQL Azure (RTM) - 12.0.2000.8
+upsert('WCSPROGRAMS_KoboBnsAnswer', 'DatasetUuidId', {
+  DatasetUuidId: dataValue('_uuid'),
+});
+
+// Refactor this for scale so it doesn't perform a no-op delete 9/10 times.
+// Maybe check result of previous op, then only delete if it was an update.
 sql({
-  query: `
-    MERGE WCSPROGRAMS_KoboBnsAnswer
-    USING (
-      VALUES ('${data._uuid}')
-    )
-    AS new (DatasetUuidId)
-    ON WCSPROGRAMS_KoboBnsAnswer.DatasetUuidId = new.DatasetUuidId 
-    WHEN MATCHED THEN
-      UPDATE SET DatasetUuidId = new.DatasetUuidId
-    WHEN NOT MATCHED THEN
-      INSERT (DatasetUuidId)
-      VALUES (new.DatasetUuidId);
-  `,
+  query: `DELETE FROM WCSPROGRAMS_KoboBnsAnswerhhmembers where AnswerId = ${state.data.AnswerId}`
 });
 
-sql(state => {
-  const { data } = state;
-  return (
-    `insert into "WCSPROGRAMS_KoboBnsAnswer" ("` +
-    [
-      'DatasetUuidId'
-    ].join('", "') +
-    `") values ('` +
-    [
-      data._uuid,
-    ]
-      .join("', '")
-      .replace(/''/g, null) +
-    `') ON CONFLICT UPDATE THE PARENT;`
-    // This VERY STRONG language in the 'on conflict' ensures that we don't
-    // create duplicate children. When a submission is reprocessed, the
-    // agreement is that whatever children appear for _that_ submission are the
-    // final children which will appear in the DB. (i.e., no historical record.)
-  );
-});
-
-sql(state => {
-  const { data } = state;
-  // state.data.hhmembers.map()
-  return (
-    `insert into "WCSPROGRAMS_KoboBnsAnswerhhmembers" ("` +
-    [
-      'AnswerId'
-    ].join('", "') +
-    `") values ('` +
-    [
-      data._uuid,
-    ]
-      .join("', '")
-      .replace(/''/g, null) +
-    `');`
-  );
+insertMany('WCSPROGRAMS_KoboBnsAnswerhhmembers', state => {
+  state.data.hhMembers.map(member => {
+    return {
+      AnswerId: state.data._uuid,
+      Name: member.name,
+    };
+  });
 });
