72b980bf962c983e65f4abde8e46c26c6d6b8bde
aleksa-krolls <aleksagk@gmail.com>
2020-06-29 12:43:59 -0500

    updated mappings to include 2 upsert statements

--- ngrt/2017.js
+++ ngrt/2017.js
@@ -11,32 +11,25 @@ upsert('WCSPROGRAMS_KoboNrgtNrgtanswer', 'DatasetUuidId', {
   // more: dataValue('moreFields'),
 });
 
-// Refactor this for scale so it doesn't perform a no-op delete 9/10 times.
-// Maybe check result of previous op, then only delete if it was an update.
-sql({
-  query: `DELETE FROM WCSPROGRAMS_KoboNrgtNrgtanswergs where AnswerId = ${state.data.body. AnswerId}`,
-});
-
-insertMany('WCSPROGRAMS_KoboNrgtNrgtanswergs', state => {
-  state.data.nrtAns.map(member => {
-    return {
-      AnswerId: state.data._id,
-      SurveyDate: state.data.body._submission_time,
-      Code: member.code,
-      Gender: member.gender,
-      Member: member.member,
-      Legitimacy: member.legitimacy,
-      Accountability: member.accountability,
-      Transparency: member.transparency,
-      Participation: member.participation,
-      Fairness: member.fairness,
-      KnowledgeSkills: member.knowledge_skills,
-      Resources: member.resources,
-      InstutionalFramework: member.institutional_framework,
-      Motivation: member.motivation,
-      EnactDecision: member.enact_decision,
-      HeldAccountable: member.held_accountable,
-      Diversity: member.diversity,
-    };
-  });
+upsert('WCSPROGRAMS_KoboNrgtNrgtanswergs', 'DatasetUuidId', {
+  DatasetUuidId: dataValue('_uuid'),
+  AnswerId:  dataValue('body._id'),
+  SurveyDate:  dataValue('body._submission_time'),
+  Code:  dataValue('body.code'),
+  Gender:  dataValue('body.gender'),
+  Member:  dataValue('body.member'),
+  Legitimacy:  dataValue('body.legitimacy'),
+  Accountability:  dataValue('body.accountability'),
+  Transparency:  dataValue('body.transparency'),
+  Participation:  dataValue('body.participation'),
+  Fairness:  dataValue('body.fairness'),
+  KnowledgeSkills:  dataValue('body.knowledge_skills'),
+  Resources:  dataValue('body.resources'),
+  InstutionalFramework:  dataValue('body.institutional_framework'),
+  Motivation:  dataValue('body.motivation'),
+  EnactDecision:  dataValue('body.enact_decision'),
+  HeldAccountable:  dataValue('body.held_accountable'),
+  Diversity:  dataValue('body.diversity'),
+  //LastUpdate: dataValue('body._submission_time')
+  // more: dataValue('moreFields'),
 });
