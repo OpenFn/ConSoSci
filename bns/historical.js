f33b0ba7ff01a3cc4df88d1be977ebd57b7e5888
Taylor Downs <taylor@openfn.org>
2020-06-30 17:44:30 -0600

    taylor@openfn.org's change on OpenFn.org

--- bns/historical.js
+++ bns/historical.js
@@ -1,9 +1,11 @@
+// Get old Kobo BNS data
 get('https://kf.kobotoolbox.org/api/v2/assets/aMpW7wwRBRbtCfidtK2rRn/data/?format=json', {}, state => {
   console.log(`Fetched ${state.data.count} submissions.`);
   state.data.submissions = state.data.results;
   return state;
 });
 
+// Then post it to the inbox to use existing flows
 each(
   dataPath('submissions[*]'),
   post(
