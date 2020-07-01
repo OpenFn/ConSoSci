00e986a980d3bbc6ffa41308c247506dd67d8c53
aleksa-krolls <aleksagk@gmail.com>
2020-07-01 09:04:09 -0500

    added insertMany for repeat group

--- bns/price.js
+++ bns/price.js
@@ -23,15 +23,33 @@ alterState(state => {
   return state;
 });
 
-upsert('WCSPROGRAMS_KoboBnsPrice', 'DatasetUuidId', {
-  DatasetUuidId: state.data._uuid,
-  Id: state.data._id,
-  Surveyor: state.data.surveyor,
-  Village: state.data.village,
-  Gs: state.data.good[0][`good/name`], //repeat group --> to update
-  Price: state.data.good[0][`good/price`], //repeat group --> to update
-  LastUpdate: state.data._submission_time, //Cannot insert the value NULL into column 'LastUpdate'--> use submission time?
+// Refactor this for scale so it doesn't perform a no-op delete 9/10 times.
+// Maybe check result of previous op, then only delete if it was an update.
+sql({ query: `DELETE FROM WCSPROGRAMS_KoboBnsPrice where AnswerId = ${state.data.AnswerId}` });
+insertMany('WCSPROGRAMS_KoboBnsPrice', state => {
+  state.data.good.map(good => {
+    return {
+      AnswerId: state.data.body._id, 
+      DatasetUuidId: state.data._uuid,
+      Id: state.data._id,
+      Surveyor: state.data.surveyor,
+      Village: state.data.village,
+      Gs: good[`good/name`],
+      Price: good[`good/price`], //repeat group --> to update
+      LastUpdate: state.data._submission_time,
+    };
+  });
 });
+
+// upsert('WCSPROGRAMS_KoboBnsPrice', 'DatasetUuidId', {
+//   DatasetUuidId: state.data._uuid,
+//   Id: state.data._id,
+//   Surveyor: state.data.surveyor,
+//   Village: state.data.village,
+//   Gs: state.data.good[0][`good/name`], //repeat group --> to update
+//   Price: state.data.good[0][`good/price`], //repeat group --> to update
+//   LastUpdate: state.data._submission_time, //Cannot insert the value NULL into column 'LastUpdate'--> use submission time?
+// });
 
 //Need a `for each` option in LP. Something like...
 // each(
