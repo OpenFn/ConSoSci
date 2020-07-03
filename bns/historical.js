5b4ba934af04a70baed8d3482daf3bfb0ec9ae79
Taylor Downs <downs.taylor@gmail.com>
2020-07-03 12:15:49 -0600

    for #8. NB that we still need a cursor

--- bns/historical.js
+++ bns/historical.js
@@ -1,36 +1,46 @@
-// Get old Kobo BNS data
-get('https://kf.kobotoolbox.org/api/v2/assets/aMpW7wwRBRbtCfidtK2rRn/data/?format=json', {}, state => {
-  console.log(`Fetched ${state.data.count} submissions.`);
-  state.data.submissions = state.data.results;
+alterState(state => {
+  state.data.surveys = [
+    'aMpW7wwRBRbtCfidtK2rRn',
+    // 'add more ids to this list',
+    // 'to perform more daily syncs',
+  ].map(id => ({
+    formId: id,
+    url: `https://kf.kobotoolbox.org/api/v2/assets/${id}/data/?format=json`,
+  }));
   return state;
 });
 
-// Then post it to the inbox to use existing flows
-each(
-  dataPath('submissions[*]'),
-  post(
-    state.configuration.openfnInboxUrl,
-    {
-      body: state => ({ form: 'bns_2019', body: state.data }),
-    },
-    state => {
-      const delay = 2000;
-      console.log(`Waiting ${delay}ms. ⏱️`);
-      function timer() {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            resolve('Done. 👍');
-          }, delay);
-        });
-      }
+each(dataPath('surveys[*]'), state =>
+  get(state.data.url, {}, state => {
+    console.log(`Fetched ${state.data.count} submissions.`);
+    state.data.submissions = state.data.results;
+    return each(
+      dataPath('submissions[*]'),
+      post(
+        state.configuration.openfnInboxUrl,
+        {
+          body: state => ({ form: 'bns_2019', body: state.data }),
+        },
+        state => {
+          const delay = 1000;
+          console.log(`Waiting ${delay}ms. ⏱️`);
+          function timer() {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve('Done. 👍');
+              }, delay);
+            });
+          }
 
-      async function msg() {
-        const msg = await timer();
-        console.log(msg);
-        return state;
-      }
+          async function msg() {
+            const msg = await timer();
+            console.log(msg);
+            return state;
+          }
 
-      return msg();
-    }
-  )
+          return msg();
+        }
+      )
+    )(state);
+  })(state)
 );
