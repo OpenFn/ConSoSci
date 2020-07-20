dab5d66a34e7ae35d871a1fac0b150e2b9cb5c03
Aleksa Krolls <aleksagk@gmail.com>
2020-07-21 02:56:17 +0300

    Updating logs

--- bns/historical.js
+++ bns/historical.js
@@ -1,24 +1,23 @@
-//== Job to be used for historical, once-off migrations  ==//
+//== Job to be used for fetching data from Kobo for (1) ongoing syncs and (2) historical, once-off migrations  ==//
 // This can be run on-demand at any time by clicking "run" //
 
-//NOTE: This job is currently configured to support BNS historical migration, but another version is in development to
-//support other survey types. Please test migrating historical BNS forms for now.
 alterState(state => {
   state.data.surveys = [
-    { id: 'aMpW7wwRBRbtCfidtK2rRn', tag: 'bns_2019' }, //Form Id of test OpenFN BNS Survey form
+    { id: 'aMpW7wwRBRbtCfidtK2rRn', tag: 'bns_2019' }, //Form Id, Tag of test OpenFN BNS Survey form
     // Historical BNS forms to migrate:
-    // { id: 'ad7S4hPBN7qM4Ac3mpEdwZ', tag: 'foo' },
-    // { id: 'amD3cUsR4Jurj3ZSUyQdBH', tag: 'foo' },
-    // { id: 'a2m5Hp4BoN956CBxfKHdJY', tag: 'foo' },
-    // { id: 'aFQWAYHXXfh8i2cmXw9BFi', tag: 'foo' },
-    // { id: 'auqBASwJMLvkqtAJnZ8SXx', tag: 'foo' },
-    // { id: 'a9R68Er4oeDx6quZre2DM7', tag: 'foo' },
-    // { id: 'arJkDGmkhhCiJ2eYwRcCit', tag: 'foo' },
-    // { id: 'aEypYtcfNGvDtpkSUPXrJN', tag: 'foo' },
-    // { id: 'aCShrrKNApccvaAPzxzbxK', tag: 'foo' },
-    // { id: 'aQbjGLfvPTEUjdTmsdTu46', tag: 'foo' },
-    // { id: 'ahz5DN45juUzp7eUfGS5QA', tag: 'foo' },
-    // Add more ids and tags to this list to perform more daily syncs.
+    // { id: 'ad7S4hPBN7qM4Ac3mpEdwZ', tag: 'bns_2019' }, //Make sure the tag matches the Trigger of the related Job
+    // { id: 'amD3cUsR4Jurj3ZSUyQdBH', tag: 'bns_2019' },
+    // { id: 'a2m5Hp4BoN956CBxfKHdJY', tag: 'bns_2019' },
+    // { id: 'aFQWAYHXXfh8i2cmXw9BFi', tag: 'bns_2019' },
+    // { id: 'auqBASwJMLvkqtAJnZ8SXx', tag: 'bns_2019' },
+    // { id: 'a9R68Er4oeDx6quZre2DM7', tag: 'bns_2019' },
+    // { id: 'arJkDGmkhhCiJ2eYwRcCit', tag: 'bns_2019' },
+    // { id: 'aEypYtcfNGvDtpkSUPXrJN', tag: 'bns_2019' },
+    // { id: 'aCShrrKNApccvaAPzxzbxK', tag: 'bns_2019' },
+    // { id: 'aQbjGLfvPTEUjdTmsdTu46', tag: 'bns_2019' },
+    // { id: 'ahz5DN45juUzp7eUfGS5QA', tag: 'bns_2019' },
+    // Add more Form ids and tags to this list to perform more Kobo syncs...
+    // { id: 'apnzrb2RuoKu8Uxy2svwF6', tag: 'nrgt_2017' },
   ].map(survey => ({
     formId: survey.id,
     tag: survey.tag,
@@ -30,14 +29,12 @@ alterState(state => {
 each(dataPath('surveys[*]'), state =>
   get(state.data.url, {}, state => {
     state.data.submissions = state.data.results.map(submission => ({
-      //Here we add form name tag to trigger the other jobs
-      //Because bns survey.js job trigger is currently configured to run
-      //for any survey with this {form: 'bns_2019'} tag.
+      //Here we append the tags defined above to the Kobo form submission data
       form: lastReferenceValue('tag')(state),
       body: submission,
     }));
     console.log(`Fetched ${state.data.count} submissions.`);
-    //Once we fetch the data, we want to post each individual survey
+    //Once we fetch the data, we want to post each individual Kobo survey
     //back to the OpenFn inbox to run through the jobs
     return each(
       dataPath('submissions[*]'),
