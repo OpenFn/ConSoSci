ffcb0de2d8ed0201f7ab234a1850acf557dda82e
Taylor Downs <downs.taylor@gmail.com>
2020-09-22 15:42:25 +0100

    progress

--- bns/historical.js
+++ bns/historical.js
@@ -16,26 +16,27 @@ alterState(state => {
       // { id: 'aMpW7wwRBRbtCfidtK2rRn', tag: 'bns_2019' }, // ✓
       // { id: 'atyo55YdBdfxzXiaBdrbvr', tag: 'bns_2019' }, // ✓
       // { id: 'aTRKQW2b8TJGxF7DVPfjFv', tag: 'bns_price_2019' }, // ✓
-      // { id: 'ad7S4hPBN7qM4Ac3mpEdwZ', tag: 'bns_price_2019' }, // This isn't bns_price_2019?
+      // { id: 'ad7S4hPBN7qM4Ac3mpEdwZ', tag: 'bns_2019' }, // ✓ changed by j
       // { id: 'amD3cUsR4Jurj3ZSUyQdBH', tag: 'bns_2019' }, // ✓
       // { id: 'a2m5Hp4BoN956CBxfKHdJY', tag: 'bns_2019' }, // ✓
+      // { id: 'aFQWAYHXXfh8i2cmXw9BFi', tag: 'bns_2019' }, // ✓
       // NOTE: The above have been finished. ===================================
-      // { id: 'aFQWAYHXXfh8i2cmXw9BFi', tag: 'bns_2019' },
-      // { id: 'auqBASwJMLvkqtAJnZ8SXx', tag: 'bns_2019' },
-      // { id: 'a9R68Er4oeDx6quZre2DM7', tag: 'bns_2019' },
-      // { id: 'arJkDGmkhhCiJ2eYwRcCit', tag: 'bns_2019' },
-      // { id: 'aEypYtcfNGvDtpkSUPXrJN', tag: 'bns_2019' },
-      // { id: 'aCShrrKNApccvaAPzxzbxK', tag: 'bns_2019' },
-      // { id: 'aQbjGLfvPTEUjdTmsdTu46', tag: 'bns_2019' },
-      // { id: 'ahz5DN45juUzp7eUfGS5QA', tag: 'bns_2019' },
+      { id: 'auqBASwJMLvkqtAJnZ8SXx', tag: 'bns_2019' },
+      { id: 'a9R68Er4oeDx6quZre2DM7', tag: 'bns_2019' },
+      { id: 'arJkDGmkhhCiJ2eYwRcCit', tag: 'bns_2019' },
+      { id: 'aEypYtcfNGvDtpkSUPXrJN', tag: 'bns_2019' },
+      { id: 'aCShrrKNApccvaAPzxzbxK', tag: 'bns_2019' },
+      { id: 'aQbjGLfvPTEUjdTmsdTu46', tag: 'bns_2019' },
+      { id: 'ahz5DN45juUzp7eUfGS5QA', tag: 'bns_2019' },
+      // NOTE: The below have been finished. ===================================
       // { id: 'a6BjZ8ncLtTtfBSGZ9PqoJ', tag: 'bns_price_2019' }, // ✓
       // { id: 'awQmCEf63g5KN2G4kcBWrc', tag: 'bns_price_2019' }, // ✓
       // { id: 'apMTFWRd9fQWHvpJHLfvk6', tag: 'bns_price_2019' }, // ✓
       // { id: 'ao52kp6BgLgooE3MRsdy4B', tag: 'bns_price_2019' }, // ✓
       // { id: 'aq5r9cKQYBRDT9SBqYanUP', tag: 'bns_price_2019' }, // ✓
-      // { id: 'auPGpyWbn4PhTuWFBfKYES', tag: 'bns_2019' },
-      // { id: 'azepksQ62i8vETEzUfz8jj', tag: 'bns_2019' },
-      // { id: 'acK4WZ2ueqk8NvUdwctxz8', tag: 'bns_2019' },
+      // { id: 'auPGpyWbn4PhTuWFBfKYES', tag: 'bns_2019' }, // ✓
+      // { id: 'azepksQ62i8vETEzUfz8jj', tag: 'bns_2019' }, // ✓
+      // { id: 'acK4WZ2ueqk8NvUdwctxz8', tag: 'bns_2019' }, // ✓
       // { id: 'apnzrb2RuoKu8Uxy2svwF6', tag: 'nrgt_2017' }, // ✓
       // { id: 'aeWW3VLbdMDgUHHdoRCUH9', tag: 'nrgt_2017' }, // ✓
       // { id: 'aZZV4KikgRKz79LqqQR5Ma', tag: 'nrgt_2017' }, // ✓
@@ -44,7 +45,6 @@ alterState(state => {
       // { id: 'aU6gbhMjfHyGekAma8wHhG', tag: 'nrgt_2017' }, // ✓
       // { id: 'asRbCJuyX3KJMFkB2p9Hh7', tag: 'nrgt_2017' }, // ✓
       // { id: 'ajDeQVDrz2AZxvxLLWjiBE', tag: 'nrgt_2019' }, // ✓
-      // NOTE: The below have been finished. ===================================
       // { id: 'aLhFXhkmM4rZJvtqiSjChE', tag: 'nrgt_2017' }, // ✓
       // { id: 'aTkDkjcfNN7vQJdZeJHkJq', tag: 'bns_price_2019' }, // ✓
       // { id: 'awAV28ebngN7GTV2nqmyKU', tag: 'bns_price_2019' }, // ✓
@@ -79,9 +79,12 @@ each(dataPath('surveys[*]'), state => {
     console.log(`Fetched ${count} submissions from ${formId} (${tag}).`);
     //Once we fetch the data, we want to post each individual Kobo survey
     //back to the OpenFn inbox to run through the jobs =========================
-    // return state;
     return each(dataPath('submissions[*]'), state => {
       console.log(`Posting ${state.data.i + 1} of ${count}...`);
+      // if (state.data.i < 920) {
+      //   console.log('Skpping post, already done.');
+      //   return state;
+      // }
       return post(state.configuration.openfnInboxUrl, { body: state => state.data })(state);
     })(state);
     // =========================================================================
