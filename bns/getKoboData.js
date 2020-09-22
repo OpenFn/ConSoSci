8d17ebbb91f322ed7bcbca15a237f83d83624873
Taylor Downs <downs.taylor@gmail.com>
2020-09-22 12:24:07 +0100

    bring new work to ongoing job

--- bns/getKoboData.js
+++ bns/getKoboData.js
@@ -5,7 +5,8 @@ alterState(state => {
   // Set a manual cursor if you'd like to only fetch data after this date.
   manualCursor = '2020-05-25T14:32:43.325+01:00';
   state.data = {
-    surveys: [ //Tag options: nrgt_2017, nrgt_2019, bns_2019, bns_price_2019
+    surveys: [
+      //Tag options: nrgt_2017, nrgt_2019, bns_2019, bns_price_2019
       { id: 'aMpW7wwRBRbtCfidtK2rRn', tag: 'bns_2019' }, //Form Id, Tag of test OpenFN BNS Survey form --> For Testing
       // Add more Form ids and tags to this list to perform more Kobo syncs...
       //{ id: 'atyo55YdBdfxzXiaBdrbvr', tag: 'bns_2019' }, //BNS Price survey to be synced regularly
@@ -20,40 +21,29 @@ alterState(state => {
   return state;
 });
 
-each(dataPath('surveys[*]'), state =>
-  get(`${state.data.url}${state.data.query}`, {}, state => {
-    state.data.submissions = state.data.results.map(submission => ({
-      //Here we append the tags defined above to the Kobo form submission data
-      form: lastReferenceValue('tag')(state),
-      body: submission,
-    }));
-    console.log(`Fetched ${state.data.count} submissions.`);
+each(dataPath('surveys[*]'), state => {
+  const { url, tag, formId } = state.data;
+  return get(url, {}, state => {
+    state.data.submissions = state.data.results.map((submission, i) => {
+      return {
+        i,
+        // Here we append the tags defined above to the Kobo form submission data
+        form: tag,
+        body: submission,
+      };
+    });
+    const count = state.data.submissions.length;
+    console.log(`Fetched ${count} submissions from ${formId} (${tag}).`);
     //Once we fetch the data, we want to post each individual Kobo survey
-    //back to the OpenFn inbox to run through the jobs
-    return each(
-      dataPath('submissions[*]'),
-      post(state.configuration.openfnInboxUrl, { body: state => state.data }, state => {
-        const delay = 1000;
-        console.log(`Waiting ${delay}ms. ⏱️`);
-        function timer() {
-          return new Promise(resolve => {
-            setTimeout(() => {
-              resolve('Done. 👍');
-            }, delay);
-          });
-        }
-
-        async function msg() {
-          const msg = await timer();
-          console.log(msg);
-          return state;
-        }
-
-        return msg();
-      })
-    )(state);
-  })(state)
-);
+    //back to the OpenFn inbox to run through the jobs =========================
+    // return state;
+    return each(dataPath('submissions[*]'), state => {
+      console.log(`Posting ${state.data.i + 1} of ${count}...`);
+      return post(state.configuration.openfnInboxUrl, { body: state => state.data })(state);
+    })(state);
+    // =========================================================================
+  })(state);
+});
 
 alterState(state => {
   const lastEnd = state.references
