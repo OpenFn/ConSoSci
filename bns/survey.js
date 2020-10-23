fae917146bcda3ed884585943d1f680958379052
Taylor Downs <taylor@openfn.org>
2020-10-23 08:06:55 +0100

    rita@openfn.org's change on OpenFn.org

--- bns/survey.js
+++ bns/survey.js
@@ -116,9 +116,9 @@ sql({
   query: state =>
     `DELETE FROM WCSPROGRAMS_KoboBnsAnswerhhmembers where Id = ${state.data._id}`,
 });
-insert('WCSPROGRAMS_KoboBnsAnswerhhmembers', {
+insert('WCSPROGRAMS_KoboBnsAnswerhhmembers', 'DatasetUuidId', {
   //insert hh head first
-  //DatasetUuidId: dataValue('durableUUID'),
+  DatasetUuidId: dataValue('durableUUID'),
   Id: dataValue('._id'),
   AnswerId: dataValue('._id'), //Q: replace with AnswerId ?
   Head: dataValue('gender_head') ? '1' : '0',
