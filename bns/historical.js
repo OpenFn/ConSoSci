d7a30f0d8f82a12a2485755279a3269c24f9ec10
Aleksa Krolls <aleksagk@gmail.com>
2020-10-25 12:16:22 +0200

    aleksa@openfn.org's change on OpenFn.org

--- bns/historical.js
+++ bns/historical.js
@@ -47,11 +47,11 @@ alterState(state => {
       // { id: 'aTkDkjcfNN7vQJdZeJHkJq', tag: 'bns_survey', name: 'Price Makira 2017', owner: 'wcs_mamabaie' }, // fetched Oct25 ✓  
       // { id: 'awAV28ebngN7GTV2nqmyKU', tag: 'bns_survey', name: 'Price Makira 2019', owner: 'wcs_mamabaie' }, // fetched Oct25 ✓ 
       // { id: 'aqwNyk7ikXxU9x4u77YfnS', tag: 'bns_survey', name: 'Prices Crossriver 2017 ', owner: 'wcs_crossriver' }, // fetched Oct25 ✓  
-       { id: 'aiRKYSPhXEhzkLtjPQbD9x', tag: 'bns_survey', name: 'Prices MTKB 2017', owner: 'akirkby' }, // 
-       { id: 'aJZxvpgS73vJu4NUxTtvwJ', tag: 'bns_survey', name: 'Prix_BNS_ABS_2019', owner: 'wcs_antongil' }, // 
-       { id: 'a2bwTreEbymbWD3JGJ2qXT', tag: 'bns_survey', name: 'Prix_BNS_Ankarea/Ankivony_2019', owner: 'wcs_soariake' }, // 
-       { id: 'aj67aaDZa52oLBFPVGWWwu', tag: 'bns_survey', name: 'Prix_BNS_Nosy Be_2020', owner: 'wcs_soariake' }, // 
-       { id: 'av3SpGmYTBP9A6dLMbzhZR', tag: 'bns_survey', name: 'Prix_BNS_Soariake_2019', owner: 'wcs_soariake' }, // 
+      // { id: 'aiRKYSPhXEhzkLtjPQbD9x', tag: 'bns_survey', name: 'Prices MTKB 2017', owner: 'akirkby' }, // fetched Oct25 ✓  
+      // { id: 'aJZxvpgS73vJu4NUxTtvwJ', tag: 'bns_survey', name: 'Prix_BNS_ABS_2019', owner: 'wcs_antongil' }, // fetched Oct25 ✓  
+      // { id: 'a2bwTreEbymbWD3JGJ2qXT', tag: 'bns_survey', name: 'Prix_BNS_Ankarea/Ankivony_2019', owner: 'wcs_soariake' }, // fetched Oct25 ✓  
+      // { id: 'aj67aaDZa52oLBFPVGWWwu', tag: 'bns_survey', name: 'Prix_BNS_Nosy Be_2020', owner: 'wcs_soariake' }, // fetched Oct25 ✓  
+      // { id: 'av3SpGmYTBP9A6dLMbzhZR', tag: 'bns_survey', name: 'Prix_BNS_Soariake_2019', owner: 'wcs_soariake' }, // fetched Oct25 ✓  
     ].map(survey => ({
       formId: survey.id,
       tag: survey.tag,
