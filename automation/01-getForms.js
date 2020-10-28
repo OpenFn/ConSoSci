615a3c30848f15829142fdf81664b22851d903db
Taylor Downs <taylor@openfn.org>
2020-10-28 18:26:31 +0000

    taylor@openfn.org's change on OpenFn.org

--- automation/01-getForms.js
+++ automation/01-getForms.js
@@ -3,9 +3,7 @@ get('https://kf.kobotoolbox.org/api/v2/assets/?format=json', {}, state => {
   // Set a manual cursor if you'd like to only fetch form after a certain date
   const manualCursor = '2019-05-25T14:32:43.325+01:00';
   state.data.forms = state.data.results
-    .filter(
-      resource => resource.date_modified > (state.lastEnd || manualCursor)
-    )
+    .filter(resource => resource.date_modified > (state.lastEnd || manualCursor))
     .map(form => {
       const url = form.url.split('?').join('?');
       return {
@@ -46,16 +44,11 @@ each(
         select_one: 'varchar',
         start: 'date',
         text: 'text',
-        today: 'date'
+        today: 'date',
+        jsonb: 'jsonb',
       };
 
-      const discards = [
-        'begin_group',
-        'begin_repeat',
-        'end_group',
-        'end_repeat',
-        'note',
-      ];
+      const discards = ['begin_group', 'begin_repeat', 'end_group', 'end_repeat', 'note'];
 
       function questionToType(questions) {
         var form = questions.filter(elt => !discards.includes(elt.type));
@@ -63,6 +56,10 @@ each(
         form.forEach(obj => {
           if (obj.name === 'group') {
             obj.name = 'kobogroup';
+          }
+          if (obj.name == 'end') {
+            // end is reserved in postgresql
+            obj.name = 'end_date';
           }
         });
         form = form
@@ -73,6 +70,8 @@ each(
             return x;
           })
           .filter(x => x.name !== undefined);
+        // Adding a column as jsonb to take the whole payload
+        form.push({ name: 'payload', type: 'jsonb' });
 
         return form;
       }
@@ -81,32 +80,30 @@ each(
         var index_begin = -1;
         var index_end = -1;
 
-        questions.find((item, i) => {
-          if (item.type === 'begin_repeat') {
-            index_begin = i;
-          }
-          if (item.type === 'end_repeat') index_end = i;
-        });
+        index_begin = questions.lastIndexOf(questions.find(item => item.type === 'begin_repeat'));
+        index_end = questions.indexOf(questions.find(item => item.type === 'end_repeat'));
 
         if (-1 !== (index_begin | index_end)) {
-          const group = questions.splice(
-            index_begin,
-            index_end - index_begin + 1
-          );
+          const group = questions.splice(index_begin, index_end - index_begin + 1);
           tables.push({
             name: (formName + '_' + questions[index_begin].name)
-              .split(/\s|-/)
+              .split(/\s|-|'/)
               .join('_')
               .toLowerCase(),
             columns: questionToType(group),
             formDef: group,
+            group: 'repeat_group',
           });
           return tablesFromQuestions(questions, formName, tables);
         }
         tables.push({
-          name: formName.split(/\s|-/).join('_').toLowerCase(),
+          name: formName
+            .split(/\s|-|'/)
+            .join('_')
+            .toLowerCase(),
           columns: questionToType(questions),
           formDef: questions,
+          group: 'parent',
         });
         return tables;
       }
