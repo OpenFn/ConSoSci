7268503878b2225c916ec2b7940f69ca5d9096ed
Taylor Downs <downs.taylor@gmail.com>
2020-10-29 15:22:47 +0000

    know path inside every repeat group

--- automation/recursed.js
+++ automation/recursed.js
@@ -3,44 +3,50 @@ const tables = [];
 
 const questions = [
   { p: 0, type: 'surveyDate' },
-  { p: 1, type: 'begin_repeat', name: 'animaux', expectedDepth: 1 },
-  { p: 2, type: 'isCat' },
-  { p: 3, type: 'end_repeat' },
-  { p: 4, type: 'begin_repeat', name: 'enfants', expectedDepth: 1 },
+  { p: 4, type: 'begin_repeat', name: 'enfants', exp: 1 },
   { p: 5, type: 'age' },
   { p: 6, type: 'gender' },
-  { p: 7, type: 'begin_repeat', name: 'medicaments', expectedDepth: 2 },
+  { p: 7, type: 'begin_repeat', name: 'medicaments', exp: 2 },
   { p: 8, type: 'generic_name' },
   { p: 9, type: 'dose' },
-  { p: 10, type: 'begin_repeat', name: 'bad_reactions', expectedDepth: 3 },
+  { p: 10, type: 'begin_repeat', name: 'bad_reactions', exp: 3 },
   { p: 11, type: 'kind_of_reaction' },
   { p: 12, type: 'end_repeat' },
   { p: 13, type: 'lastTaken' },
   { p: 14, type: 'end_repeat' },
   { p: 15, type: 'end_repeat' },
+  { p: 1, type: 'begin_repeat', name: 'animaux', exp: 1 },
+  { p: 2, type: 'isCat' },
+  { p: 3, type: 'end_repeat' },
 ];
 
 let depth = 0;
+let progenitor = {};
+let path = [];
 
 for (let index = 0; index < questions.length; index++) {
   const q = questions[index];
   switch (q.type) {
     case 'begin_repeat':
       depth++;
+      questions[index] = { ...q, depth, parent: progenitor.name, path };
+      console.log(questions[index]);
+      progenitor = q;
+      path.push(q.name);
       break;
 
     case 'end_repeat':
       depth--;
+      var i = path.indexOf(q);
+      path.splice(i, 1);
       break;
 
     default:
       break;
   }
-  questions[index] = { ...q, calculatedDepth: depth, parent: true };
 }
 
-console.log(depth);
-console.log(questions);
+// console.log(questions);
 
 function tablesFromQuestions(questions, formName, tables) {
   const backwardsFirstBegin = questions
