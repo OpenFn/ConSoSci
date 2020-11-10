63183ecbe846eb1716234528582b91425325e017
Taylor Downs <downs.taylor@gmail.com>
2020-11-10 11:56:33 +0000

    remove catch, let error throw on 02a

--- automation/02a-syncToPostgres.js
+++ automation/02a-syncToPostgres.js
@@ -4,36 +4,41 @@ each('$.forms[*]', state => {
     alterState(state => {
       const { name } = state.data;
       if (name !== `${state.prefix1}__${state.prefix2}_Untitled`) {
-        return describeTable(name.toLowerCase(), {writeSql: true})(state)
-          .then(postgresColumn => {
-            const { rows } = postgresColumn.response.body;
-            if (postgresColumn.response.body.rowCount === 0) {
-              console.log('No matching table found in postgres --- Inserting.');
+        return describeTable(name.toLowerCase(), { writeSql: true })(
+          state
+        ).then(postgresColumn => {
+          const { rows } = postgresColumn.response.body;
+          if (postgresColumn.response.body.rowCount === 0) {
+            console.log('No matching table found in postgres --- Inserting.');
 
-              const columns = state.data.columns.filter(x => x.name !== undefined);
-              return insertTable(name, state => columns, {writeSql: true})(state);
-            } else {
-              const columnNames = rows.map(x => x.column_name);
+            const columns = state.data.columns.filter(
+              x => x.name !== undefined
+            );
+            return insertTable(name, state => columns, { writeSql: true })(
+              state
+            );
+          } else {
+            const columnNames = rows.map(x => x.column_name);
 
-              console.log('----------------------');
-              const newColumns = state.data.columns.filter(
-                x => x.name !== undefined && !columnNames.includes(x.name.toLowerCase())
+            console.log('----------------------');
+            const newColumns = state.data.columns.filter(
+              x =>
+                x.name !== undefined &&
+                !columnNames.includes(x.name.toLowerCase())
+            );
+            console.log(newColumns);
+            if (newColumns.length > 0) {
+              console.log('Existing table found in postgres --- Updating.');
+
+              return modifyTable(name, state => newColumns, { writeSql: true })(
+                state
               );
-              console.log(newColumns);
-              if (newColumns.length > 0) {
-                console.log('Existing table found in postgres --- Updating.');
-
-                return modifyTable(name, state => newColumns, {writeSql: true})(state);
-              } else {
-                console.log('No new columns to add.');
-                return state;
-              }
+            } else {
+              console.log('No new columns to add.');
+              return state;
             }
-          })
-          .catch(error => {
-            console.log('here');
-            console.log(error);
-          });
+          }
+        });
       }
       return state;
     })
