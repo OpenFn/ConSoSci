42725e08efde41e7a6a4c6233485ebd91222415a
Taylor Downs <taylor@openfn.org>
2020-11-12 07:45:50 +0000

    rita@openfn.org's change on OpenFn.org

--- automation/00-findUpdates.js
+++ automation/00-findUpdates.js
@@ -11,7 +11,6 @@ get('https://kf.kobotoolbox.org/api/v2/assets/?format=json', {}, state => {
     { uid: 'aDVDagX8TE9NUY7xmvAUpv', p1: 'WCS', p2: 'Marché' },
     { uid: 'afgHDJgMfp5cZrPbAQWerN', p1: 'WCS', p2: 'Test' },
     { uid: 'aS6QvorHkKGZm2GmrGMnY4', p1: 'WCS', p2: 'Test2' },
-    { uid: 'aS6QvorHkKGZm2GmrGMnY4', p1: 'WCS', p2: 'Test2' },
 
   ];
 
