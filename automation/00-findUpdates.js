0cb79c13091eda6083ae1ec59c741fdb818ec4d5
Taylor Downs <taylor@openfn.org>
2020-11-18 11:47:33 +0000

    rita@openfn.org's change on OpenFn.org

--- automation/00-findUpdates.js
+++ automation/00-findUpdates.js
@@ -16,7 +16,8 @@ get('https://kf.kobotoolbox.org/api/v2/assets/?format=json', {}, state => {
     { uid: 'aydKvZSY9kdnDZn6uLvqgZ', p1: 'WCS', p2: 'Cameratrapping'}, //test automation 17nov2020
     { uid: 'acZdoLnafZ5WZscgVErALo', p1: 'WCS', p2: 'Cameratrapping'}, //test automation 17nov2020 with duplicated and renamed form
     { uid: 'aMW7HQB9UVzdLN8sfDGkgN', p1: 'WCS', p2: 'Cameratrapping'}, //test automation 17nov2020 with same form name but different form
-    { uid: 'aFjWKDYJghBrc56NfGzepM', p1: 'WCS', p2: 'Cameratrapping'}
+    { uid: 'aFjWKDYJghBrc56NfGzepM', p1: 'WCS', p2: 'Cameratrapping'},
+    { uid: 'ajPZcZzsXn5VraTUWgbUci', p1: 'WCS', p2: 'KidsOR'}
   ];
 
   state.data.forms = state.data.results
