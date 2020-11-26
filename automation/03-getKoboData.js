a572ab9197537996850d38ea99294d1a38599e83
Taylor Downs <downs.taylor@gmail.com>
2020-11-26 13:27:06 +0000

    03 provides name and destination for each form

--- automation/03-getKoboData.js
+++ automation/03-getKoboData.js
@@ -9,7 +9,7 @@ alterState(state => {
   state.data = {
     surveys: [
       //** Specify new forms to fetch here **//
-      //{ id: 'acZdoLnafZ5WZscgVErALo', name: 'Form Project Name'},
+      // { id: 'acZdoLnafZ5WZscgVErALo', name: 'Form Project Name'},
       // { id: 'a83rSAZ9Cehk26SSkSJm8Z', name: 'WCS_KidsOR_V323novCopyOfKidsorDemo'},
       // { id: 'aeUjHuADwhK9RKGz8cgr5W', name: 'WCS_KidsOR_V423novCopyOfKidsorDemo'},
       // { id: 'aEAqMfzhGtgx5ZoYp3rPCb', name: 'WCS_Marche_24novSwmMarcheTest'},
@@ -17,19 +17,26 @@ alterState(state => {
       // { id: 'avW25WTT7t2LDcgeq426dM', name: 'WCS_KidsOR_24novCopyOfKidsorDemo'},
       // { id: 'aRfFb8z8HGiaUfTCZoe7t8', name: 'test_form_from_xls_2.xls'},
       // { id: 'an2x2PxChR6SjmASEFQcmh', name: 'WCS_RuralConsumption_25novWcsRuralConsumptionTest'}
-      //{ id: 'a3fpvhRAUoGPrPGZvSdzAW', name: 'BwalyaSurvey_V2'}
+      // { id: 'a3fpvhRAUoGPrPGZvSdzAW', name: 'BwalyaSurvey_V2'}
+      // {
+      //   id: 'aDVDagX8TE9NUY7xmvAUpv',
+      //   formName: 'WCS_marche_SwmEtudeMarché2020VendorSales',
+      //   destination: 'WCS_marche_marche',
+      // },
+      // { id: 'a9eJJ2hrRSMCJZ95WMc93j', name: 'WCS_swm_ConsommationUrbaineSwm'},
       {
-        id: 'aDVDagX8TE9NUY7xmvAUpv',
-        destination: 'WCS_marche_SwmEtudeMarché2020VendorSales',
+        id: 'aaayFwZcjbp8gFeYeqohHu',
+        formName: '26Nov SWM Marche Test',
+        destination: 'WCS_marche_marche',
       },
-      //{ id: 'a9eJJ2hrRSMCJZ95WMc93j', name: 'WCS_swm_ConsommationUrbaineSwm'},
-      { id: 'aaayFwZcjbp8gFeYeqohHu', destination: 'WCS_marche_marche' },
-      { id: 'atFB5uoXJtzwJoPCKtNPjg', destination: 'WCS_marche_marche' },
+      {
+        id: 'atFB5uoXJtzwJoPCKtNPjg',
+        formName: 'Nov26 WCS One-1st-Level With Chaiwa Test',
+        destination: 'WCS_marche_marche',
+      },
     ].map(survey => ({
+      ...survey,
       formId: survey.id,
-      tag: survey.tag,
-      destination: survey.destination,
-      owner: survey.owner,
       url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.id}/data/?format=json`,
       query: `&query={"end":{"$gte":"${state.lastEnd || manualCursor}"}}`,
     })),
@@ -43,9 +50,8 @@ each(dataPath('surveys[*]'), state => {
     state.data.submissions = state.data.results.map(submission => {
       return {
         // Here we append the tags defined above to the Kobo form submission data
-        form: name,
         destination,
-        formName: destination,
+        formName,
         formOwner: owner,
         body: submission,
       };
