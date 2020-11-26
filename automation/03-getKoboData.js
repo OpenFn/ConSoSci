967c5e3258627563a1dffe96170bf5eafbdf88a6
Taylor Downs <downs.taylor@gmail.com>
2020-11-26 07:37:08 +0000

    linting 03

--- automation/03-getKoboData.js
+++ automation/03-getKoboData.js
@@ -9,29 +9,26 @@ alterState(state => {
   state.data = {
     surveys: [
       //** Specify new forms to fetch here **//
-     //{ id: 'acZdoLnafZ5WZscgVErALo', name: 'Form Project Name'},
-    // { id: 'a83rSAZ9Cehk26SSkSJm8Z', name: 'WCS_KidsOR_V323novCopyOfKidsorDemo'},
-    // { id: 'aeUjHuADwhK9RKGz8cgr5W', name: 'WCS_KidsOR_V423novCopyOfKidsorDemo'},
-    // { id: 'aEAqMfzhGtgx5ZoYp3rPCb', name: 'WCS_Marche_24novSwmMarcheTest'},
-    // { id: 'aq8jbMUZ89LndzDdUiWeA4', name: 'WCS_marche_V224novSwmMarcheTest'},
-    // { id: 'avW25WTT7t2LDcgeq426dM', name: 'WCS_KidsOR_24novCopyOfKidsorDemo'},
-    // { id: 'aRfFb8z8HGiaUfTCZoe7t8', name: 'test_form_from_xls_2.xls'},
-    // { id: 'an2x2PxChR6SjmASEFQcmh', name: 'WCS_RuralConsumption_25novWcsRuralConsumptionTest'}
-    //{ id: 'a3fpvhRAUoGPrPGZvSdzAW', name: 'BwalyaSurvey_V2'}
-    { id: 'aDVDagX8TE9NUY7xmvAUpv', name: 'WCS_marche_SwmEtudeMarché2020VendorSales'},
-    //{ id: 'a9eJJ2hrRSMCJZ95WMc93j', name: 'WCS_swm_ConsommationUrbaineSwm'},
-    { id: 'aaayFwZcjbp8gFeYeqohHu', name: 'WCS_swm_26novSwmMarcheTest'},
-    { id: 'atFB5uoXJtzwJoPCKtNPjg', name: 'WCS_swm_26novSwmMarcheTest'},
-
-    
-
-    
-      
-      
+      //{ id: 'acZdoLnafZ5WZscgVErALo', name: 'Form Project Name'},
+      // { id: 'a83rSAZ9Cehk26SSkSJm8Z', name: 'WCS_KidsOR_V323novCopyOfKidsorDemo'},
+      // { id: 'aeUjHuADwhK9RKGz8cgr5W', name: 'WCS_KidsOR_V423novCopyOfKidsorDemo'},
+      // { id: 'aEAqMfzhGtgx5ZoYp3rPCb', name: 'WCS_Marche_24novSwmMarcheTest'},
+      // { id: 'aq8jbMUZ89LndzDdUiWeA4', name: 'WCS_marche_V224novSwmMarcheTest'},
+      // { id: 'avW25WTT7t2LDcgeq426dM', name: 'WCS_KidsOR_24novCopyOfKidsorDemo'},
+      // { id: 'aRfFb8z8HGiaUfTCZoe7t8', name: 'test_form_from_xls_2.xls'},
+      // { id: 'an2x2PxChR6SjmASEFQcmh', name: 'WCS_RuralConsumption_25novWcsRuralConsumptionTest'}
+      //{ id: 'a3fpvhRAUoGPrPGZvSdzAW', name: 'BwalyaSurvey_V2'}
+      {
+        id: 'aDVDagX8TE9NUY7xmvAUpv',
+        name: 'WCS_marche_SwmEtudeMarché2020VendorSales',
+      },
+      //{ id: 'a9eJJ2hrRSMCJZ95WMc93j', name: 'WCS_swm_ConsommationUrbaineSwm'},
+      { id: 'aaayFwZcjbp8gFeYeqohHu', name: 'WCS_swm_26novSwmMarcheTest' },
+      { id: 'atFB5uoXJtzwJoPCKtNPjg', name: 'WCS_swm_26novSwmMarcheTest' },
     ].map(survey => ({
       formId: survey.id,
       tag: survey.tag,
-      name: survey.name, 
+      name: survey.name,
       owner: survey.owner,
       url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.id}/data/?format=json`,
       query: `&query={"end":{"$gte":"${state.lastEnd || manualCursor}"}}`,
@@ -48,7 +45,7 @@ each(dataPath('surveys[*]'), state => {
         i,
         // Here we append the tags defined above to the Kobo form submission data
         form: name,
-        formName: name, 
+        formName: name,
         formOwner: owner,
         body: submission,
       };
@@ -59,7 +56,9 @@ each(dataPath('surveys[*]'), state => {
     //back to the OpenFn inbox to run through the jobs =========================
     return each(dataPath('submissions[*]'), state => {
       console.log(`Posting ${state.data.i + 1} of ${count}...`);
-      return post(state.configuration.openfnInboxUrl, { body: state => state.data })(state);
+      return post(state.configuration.openfnInboxUrl, {
+        body: state => state.data,
+      })(state);
     })(state);
     // =========================================================================
   })(state);
@@ -70,7 +69,7 @@ alterState(state => {
     .filter(item => item.body)
     .map(s => s.body.end)
     .sort((a, b) => new Date(b.date) - new Date(a.date))[0];
-    
+
   console.log('New cursor value:', lastEnd);
   return { ...state, data: {}, references: [], lastEnd };
 });
