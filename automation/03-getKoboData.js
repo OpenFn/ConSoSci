96b53a89ee6f7934ce685ec25221220632f5397c
Aleksa Krolls <aleksagk@gmail.com>
2020-11-27 13:01:41 +0200

    aleksa@openfn.org's change on OpenFn.org

--- automation/03-getKoboData.js
+++ automation/03-getKoboData.js
@@ -8,17 +8,12 @@ alterState(state => {
   const manualCursor = '2020-05-25T14:32:43.325+01:00';
   state.data = {
     surveys: [
-      //** Specify new forms to fetch here **//
-      // { id: 'acZdoLnafZ5WZscgVErALo', name: 'Form Project Name'},
-      // { id: 'a83rSAZ9Cehk26SSkSJm8Z', name: 'WCS_KidsOR_V323novCopyOfKidsorDemo'},
-      // { id: 'aeUjHuADwhK9RKGz8cgr5W', name: 'WCS_KidsOR_V423novCopyOfKidsorDemo'},
-      // { id: 'aEAqMfzhGtgx5ZoYp3rPCb', name: 'WCS_Marche_24novSwmMarcheTest'},
-      // { id: 'aq8jbMUZ89LndzDdUiWeA4', name: 'WCS_marche_V224novSwmMarcheTest'},
-      // { id: 'avW25WTT7t2LDcgeq426dM', name: 'WCS_KidsOR_24novCopyOfKidsorDemo'},
-      // { id: 'aRfFb8z8HGiaUfTCZoe7t8', name: 'test_form_from_xls_2.xls'},
-      // { id: 'an2x2PxChR6SjmASEFQcmh', name: 'WCS_RuralConsumption_25novWcsRuralConsumptionTest'}
-      // { id: 'a3fpvhRAUoGPrPGZvSdzAW', name: 'BwalyaSurvey_V2'}
       {
+        id: 'a9eJJ2hrRSMCJZ95WMc93j',
+        formName: 'Consommation Urbaine SWM',
+        destination: 'WCS_swm_ConsommationUrbaineSwm',
+      }
+      /*{
         id: 'aDVDagX8TE9NUY7xmvAUpv',
         formName: 'WCS_marche_SwmEtudeMarché2020VendorSales',
         destination: 'WCS_marche_marche',
@@ -28,7 +23,7 @@ alterState(state => {
         id: 'aaayFwZcjbp8gFeYeqohHu',
         formName: '26Nov SWM Marche Test',
         destination: 'WCS_marche_marche',
-      },
+      },*/
     ].map(survey => ({
       ...survey,
       formId: survey.id,
