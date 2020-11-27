24731a0d32d226af89b484c1a5ce6a32e81a6768
Taylor Downs <taylor@openfn.org>
2020-11-27 14:36:23 +0000

    chaiwa@openfn.org's change on OpenFn.org

--- automation/00-findUpdates.js
+++ automation/00-findUpdates.js
@@ -7,7 +7,7 @@ get('https://kf.kobotoolbox.org/api/v2/assets/?format=json', {}, state => {
   // == FOR ADMINS: Update the below `manualFormList` to designate which Kobo forms to sync ==//
   const manualFormList = [
    // { uid: 'ayg49kwVRoKu2aa7ck3YX2', p1: 'WCS', p2: 'norepeat', tableId: "longName"} //form: Nov26 Consommation Urbaine SWM Form without repeat group but with a long name
-      { uid: 'aydKvZSY9kdnDZn6uLvqgZ', p1: 'WCS', p2: 'test', tableId: 'ClonedeCT-1.ChecklistPréparationPiègePhoto'}
+      { uid: 'afCP2i7HmbueECdhgozGwZ', p1: 'WCS', p2: 'swm', tableId: 'EuSwmSiteSurvey'}
     
   ];
 
