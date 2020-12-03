e3f09bf2d6d9f6121cd9e9a45dac0cb9062eebdf
Taylor Downs <taylor@openfn.org>
2020-12-03 06:31:10 +0000

    rita@openfn.org's change on OpenFn.org

--- automation/00-findUpdates.js
+++ automation/00-findUpdates.js
@@ -7,7 +7,8 @@ get('https://kf.kobotoolbox.org/api/v2/assets/?format=json', {}, state => {
   // == FOR ADMINS: Update the below `manualFormList` to designate which Kobo forms to sync ==//
   const manualFormList = [
    // { uid: 'ayg49kwVRoKu2aa7ck3YX2', p1: 'WCS', p2: 'norepeat', tableId: "longName"} //form: Nov26 Consommation Urbaine SWM Form without repeat group but with a long name
-      { uid: 'a3nS8beYqoG9EiP9jwDQHn', p1: 'WCS', p2: 'swm', tableId: 'WCSNewKoboForm2'}
+      { uid: 'a3nS8beYqoG9EiP9jwDQHn', p1: 'WCS', p2: 'swm', tableId: 'WCSNewKoboForm2'},
+      { uid: 'axGKmEwWdNAqWERTMGrPpy', p1: 'WCS', p2: 'group', tableId: 'GroupTest'}
     
   ];
 
