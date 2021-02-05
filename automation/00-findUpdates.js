d53030027085093d2cfc587030fcd03721e6c105
Taylor Downs <taylor@openfn.org>
2021-02-05 10:08:15 +0000

    rita@openfn.org's change on OpenFn.org

--- automation/00-findUpdates.js
+++ automation/00-findUpdates.js
@@ -11,7 +11,7 @@ get('https://kf.kobotoolbox.org/api/v2/assets/?format=json', {}, state => {
       { uid: 'axGKmEwWdNAqWERTMGrPpy', p1: 'WCS', p2: 'group', tableId: 'GroupTest'},
       { uid: 'a3mbXtJccsMaRRC5sn8fsv', p1: 'WCS', p2: 'group', tableId: 'WCSNewKoboForm'},
       { uid: 'a2eNZrDoyreoMhCemg6R3j', p1: 'WCS', p2: 'marche', tableId: 'Dec3SWMMarcheTest'},
-      { uid: 'aREgytyFbcohkrjYLN9v33', p1: 'WCS', p2: 'marche', tableId: 'SWM EtudeMarché2020Demo'},
+      { uid: 'aREgytyFbcohkrjYLN9v33', p1: 'WCS', p2: 'marche', tableId: 'SWM EtudeMarche2020Demo'},
       { uid: 'afgEFPYcFzCEBVUX28cSKr', p1: 'WCS', p2: 'kidsor', tableId: 'KidsORDemo1'}
     
   ];
