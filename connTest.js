f451627fac09fe94e28170b1640d21c3b02838e6
mamadou <mamadou.lakhassane@gmail.com>
2021-03-04 09:40:52 +0000

    testing multiple references

--- connTest.js
+++ connTest.js
@@ -1,40 +1,36 @@
-//sql({
-// query: `
-// SELECT NAME
-//    FROM sys.objects where type='u' and name like 'WCSPROGRAMS_KoboBnsAnswer%'
-// `,
-//  });
+sql({
+  query: `
+SELECT NAME
+   FROM sys.objects where type='u' and name like 'WCSPROGRAMS_KoboBnsAnswer%'
+`,
+});
 
 //alterState(state=>{
 //console.log(JSON.stringify(state.references, null, 2));
 //return state;
 //})
 
-/*sql({
+sql({
   query: `
     SELECT *
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE TABLE_NAME = 'WCSPROGRAMS_KoboNgrtNgrtanswer'`,
-});*/
+});
 
-alterState(state => {
-  return sql({
-    query: `
+sql({
+  query: `
     SELECT WCSPROGRAMS_ProjectAnnualDataPlanID 
     FROM WCSPROGRAMS_ProjectAnnualDataPlan 
     WHERE DataSetUUIDID = '86587778'`,
-  })(state).then(state => {
-    state.configuration = {};
-    //console.log(state);
-     console.log(JSON.stringify(state.references, null, 2));
-    return state;
-  });
-
-  //return state;
 });
 
-// sql({
-//   query: `
-//     SELECT @@VERSION;
-//   `,
-// });
+sql({
+  query: `
+    SELECT @@VERSION;
+  `,
+});
+
+alterState(state => {
+  console.log(JSON.stringify(state.references, null, 2));
+  return state;
+});
