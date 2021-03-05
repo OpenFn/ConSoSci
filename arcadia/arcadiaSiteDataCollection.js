5090ff442349d734b9cfd476e07af8804eb00bc4
aleksa-krolls <aleksagk@gmail.com>
2021-03-05 18:01:53 +0200

    test dataAssistence id

--- arcadia/arcadiaSiteDataCollection.js
+++ arcadia/arcadiaSiteDataCollection.js
@@ -719,7 +719,7 @@ each(
               DataSetUUIDID: body._id + dc,
               AnswerId: body._id,
               WCSPROGRAMS_ProjectAnnualDataPlanDataSetID: datasetuuid[0].value, //fk
-              WCSPROGRAMS_DataChallengeID: '2', //state.dataChallengeMap[dc], //fk
+              WCSPROGRAMS_DataChallengeID: '2', //state.dataChallengeMap[dc], //TODO: Th
               //TODO: Update UserID_CR mappings
               UserID_CR: '0',
               UserID_LM: '0',
@@ -755,7 +755,7 @@ each(
               DataSetUUIDID: body._id + dmh,
               AnswerId: body._id,
               WCSPROGRAMS_ProjectAnnualDataPlanDataSetID: datasetuuid[0].value, //fk
-              WCSPROGRAMS_DataAssistanceID: state.dataAssistanceMap[dmh], //fk
+              WCSPROGRAMS_DataAssistanceID: '1', //state.dataAssistanceMap[dmh], //fk
               //TODO: Update UserID_CR mappings
               UserID_CR: '0',
               UserID_LM: '0',
