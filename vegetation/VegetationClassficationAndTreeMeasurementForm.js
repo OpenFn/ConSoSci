4db816323284c473fed1786033f37cb0104e02a3
Aleksa Krolls <aleksagk@gmail.com>
2021-04-09 22:04:08 +0300

    aleksa@openfn.org's change on OpenFn.org

--- vegetation/VegetationClassficationAndTreeMeasurementForm.js
+++ vegetation/VegetationClassficationAndTreeMeasurementForm.js
@@ -223,7 +223,7 @@ alterState(async state => {
         ),
       },
     })(state),
-    StartTime: dataValue('$.body.start_time'),
+    SubmissionDate: dataValue('$.body.start_time'),
     Surveydate: dataValue('$.body.surveydate'),
     Answer_ID: state.data.body._id,
     UserID_CR: '0', //TODO: Update User_ID and Address mappings?
