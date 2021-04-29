2c5361064c8747d9d9ae946de33ed08a33e6619c
aleksa-krolls <aleksagk@gmail.com>
2021-04-29 23:17:39 +0300

    filter BigTrees

--- vegetation/VegetationClassficationAndTreeMeasurementForm.js
+++ vegetation/VegetationClassficationAndTreeMeasurementForm.js
@@ -645,7 +645,7 @@ alterState(async state => {
   const uniqueTrees = Array.from(
     new Set(dataArray.map(tree => tree['tree_10cm/btspecies']))
   ).map(id => {
-    return dataArray.find(c => id === c['tree_10cm/btspecies'] && c['tree_10cm/btspecies'] !== undefined);
+    return dataArray.filter(c => id === c['tree_10cm/btspecies']);
   });
   //=================================================
 
