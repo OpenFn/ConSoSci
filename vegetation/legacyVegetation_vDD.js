d3047b149d9f0c80d2410db73008f8911ce77b3c
Taylor Downs <taylor@openfn.org>
2021-06-11 15:08:22 +0100

    ddetoeuf@wcs.org's change on OpenFn.org

--- vegetation/legacyVegetation_vDD.js
+++ vegetation/legacyVegetation_vDD.js
@@ -13,9 +13,10 @@ alterState(state => {
 });
 
 // THIS OPERATION REPLACES THE ONE BELOW IT.
-alterSate(async state  => {
+alterSate(async state => {
+  const dataArray = state.survey['general'];
   const VegMap = [];
-  for (let data of dataArray){
+  for (let data of dataArray) {
     VegMap.push({
        Answer_ID: dataValue('surveyid'),
        Surveydate: dataValue('Date of survey'),
@@ -25,7 +26,7 @@ alterSate(async state  => {
        East: dataValue('Easting'),
        North: dataValue('Northing'),
        PlotGPS: dataValue('Altitude'),
-       WCSPROGRAMS_VegetationClassID: findValue({
+       WCSPROGRAMS_VegetationClassID: await findValue({
          uuid: 'WCSPROGRAMS_VegetationClassID',
          relation: 'WCSPROGRAMS_VegetationClass',
          where: {
@@ -33,7 +34,8 @@ alterSate(async state  => {
              dataValue('Vegetation_type')(state)
            ),
          },
-       }), // new line added and in following findValue as well/       WCSPROGRAMS_VegetationForestTypeID: await findValue({
+       }),        
+       WCSPROGRAMS_VegetationForestTypeID: await findValue({
          uuid: 'WCSPROGRAMS_VegetationForestTypeID',
          relation: 'WCSPROGRAMS_VegetationForestType',
          where: {
@@ -67,174 +69,67 @@ alterSate(async state  => {
          where: {
            WCSPROGRAMS_VegetationDrainageExtCode: state.handleValue(
              dataValue('Drainage')(state)
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationSoilDescriptionID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilDescriptionID',
-//         relation: 'WCSPROGRAMS_VegetationSoilDescription',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilDescriptionName: state.handleValue(
-//             dataValue('Soil description')(state)
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationSoilColorID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilColorID',
-//         relation: 'WCSPROGRAMS_VegetationSoilColor',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilColorName: state.handleValue(
-//             dataValue('Soil_colour')(state)
-//           ),
-//         },
-//       }),
-//       IsEvidenceOfFire: dataValue('Evidence_fire'),
-//       WCSPROGRAMS_VegetationSoilMoistureID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilMoistureID',
-//         relation: 'WCSPROGRAMS_VegetationSoilMoisture',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilMoistureName: state.handleValue(
-//             dataValue('Soil_Moisture')(state) ||
-//               dataValue('$.body.groundtruthing/moisture')(state) ||
-//               ''
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationSoilErodabilityID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilErodabilityID',
-//         relation: 'WCSPROGRAMS_VegetationSoilErodability',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilErodabilityExtCode: state.handleValue(
-//             dataValue('Soil Erodability')(state)
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationSoilSeasonalityID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilSeasonalityID',
-//         relation: 'WCSPROGRAMS_VegetationSoilSeasonality',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilSeasonalityName: state.handleValue(
-//             dataValue('Soil Seasonality')(state)
-//           ),
-//         },
-//       }),
-//       Bareground: dataValue('Bare ground %'),
+         ),
+         },
+       }),
+     WCSPROGRAMS_VegetationSoilDescriptionID: await findValue({
+         uuid: 'WCSPROGRAMS_VegetationSoilDescriptionID',
+         relation: 'WCSPROGRAMS_VegetationSoilDescription',
+         where: {
+           WCSPROGRAMS_VegetationSoilDescriptionName: state.handleValue(
+             dataValue('Soil description')(state)
+           ),
+         },
+       }),
+       WCSPROGRAMS_VegetationSoilColorID: await findValue({
+         uuid: 'WCSPROGRAMS_VegetationSoilColorID',
+         relation: 'WCSPROGRAMS_VegetationSoilColor',
+         where: {
+           WCSPROGRAMS_VegetationSoilColorName: state.handleValue(
+             dataValue('Soil_colour')(state)
+           ),
+         },
+       }),
+       IsEvidenceOfFire: dataValue('Evidence_fire'),
+       WCSPROGRAMS_VegetationSoilMoistureID: await findValue({
+         uuid: 'WCSPROGRAMS_VegetationSoilMoistureID',
+         relation: 'WCSPROGRAMS_VegetationSoilMoisture',
+         where: {
+           WCSPROGRAMS_VegetationSoilMoistureName: state.handleValue(
+             dataValue('Soil_Moisture')(state) ||
+               dataValue('$.body.groundtruthing/moisture')(state) ||
+               ''
+           ),
+         },
+       }),
+       WCSPROGRAMS_VegetationSoilErodabilityID: await findValue({
+         uuid: 'WCSPROGRAMS_VegetationSoilErodabilityID',
+         relation: 'WCSPROGRAMS_VegetationSoilErodability',
+         where: {
+          WCSPROGRAMS_VegetationSoilErodabilityExtCode: state.handleValue(
+             dataValue('Soil Erodability')(state)
+           ),
+         },
+}),
+       WCSPROGRAMS_VegetationSoilSeasonalityID: await findValue({
+         uuid: 'WCSPROGRAMS_VegetationSoilSeasonalityID',
+         relation: 'WCSPROGRAMS_VegetationSoilSeasonality',
+         where: {
+           WCSPROGRAMS_VegetationSoilSeasonalityName: state.handleValue(
+             dataValue('Soil Seasonality')(state)
+           ),
+         },
+       }),
+       Bareground: dataValue('Bare ground %'),
+     });
   }
-  )
-// each(
-//   'survey.general[*]',
-//   alterState(async state => {
-//     const VegMap = {
-//       Answer_ID: dataValue('surveyid'),
-//       Surveydate: dataValue('Date of survey'),
-//       SurveySite: dataValue('Survey site'),
-//       TransectNo: dataValue('Transect No'),
-//       PlotNumber: dataValue('Plot No'),
-//       East: dataValue('Easting'),
-//       North: dataValue('Northing'),
-//       PlotGPS: dataValue('Altitude'),
-//       WCSPROGRAMS_VegetationClassID: findValue({
-//         uuid: 'WCSPROGRAMS_VegetationClassID',
-//         relation: 'WCSPROGRAMS_VegetationClass',
-//         where: {
-//           WCSPROGRAMS_VegetationClassName: state.handleValue(
-//             dataValue('Vegetation_type')(state)
-//           ),
-//         },
-//       }), // new line added and in following findValue as well
-//       WCSPROGRAMS_VegetationForestTypeID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationForestTypeID',
-//         relation: 'WCSPROGRAMS_VegetationForestType',
-//         where: {
-//           WCSPROGRAMS_VegetationForestTypeExtCode: state.handleValue(
-//             dataValue('Vegetation_field')(state)
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationPhysiographyID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationPhysiographyID',
-//         relation: 'WCSPROGRAMS_VegetationPhysiography',
-//         where: {
-//           WCSPROGRAMS_VegetationPhysiographyExtCode: state.handleValue(
-//             dataValue('Physiography')(state)
-//           ),
-//         },
-//       }),
-//       //SpecimenPhoto: dataValue('Photos'),
-//       WCSPROGRAMS_VegetationTopographyID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationTopographyID',
-//         relation: 'WCSPROGRAMS_VegetationTopography',
-//         where: {
-//           WCSPROGRAMS_VegetationTopographyExtCode: state.handleValue(
-//             dataValue('Topography')(state)
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationDrainageID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationDrainageID',
-//         relation: 'WCSPROGRAMS_VegetationDrainage',
-//         where: {
-//           WCSPROGRAMS_VegetationDrainageExtCode: state.handleValue(
-//             dataValue('Drainage')(state)
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationSoilDescriptionID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilDescriptionID',
-//         relation: 'WCSPROGRAMS_VegetationSoilDescription',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilDescriptionName: state.handleValue(
-//             dataValue('Soil description')(state)
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationSoilColorID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilColorID',
-//         relation: 'WCSPROGRAMS_VegetationSoilColor',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilColorName: state.handleValue(
-//             dataValue('Soil_colour')(state)
-//           ),
-//         },
-//       }),
-//       IsEvidenceOfFire: dataValue('Evidence_fire'),
-//       WCSPROGRAMS_VegetationSoilMoistureID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilMoistureID',
-//         relation: 'WCSPROGRAMS_VegetationSoilMoisture',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilMoistureName: state.handleValue(
-//             dataValue('Soil_Moisture')(state) ||
-//               dataValue('$.body.groundtruthing/moisture')(state) ||
-//               ''
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationSoilErodabilityID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilErodabilityID',
-//         relation: 'WCSPROGRAMS_VegetationSoilErodability',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilErodabilityExtCode: state.handleValue(
-//             dataValue('Soil Erodability')(state)
-//           ),
-//         },
-//       }),
-//       WCSPROGRAMS_VegetationSoilSeasonalityID: await findValue({
-//         uuid: 'WCSPROGRAMS_VegetationSoilSeasonalityID',
-//         relation: 'WCSPROGRAMS_VegetationSoilSeasonality',
-//         where: {
-//           WCSPROGRAMS_VegetationSoilSeasonalityName: state.handleValue(
-//             dataValue('Soil Seasonality')(state)
-//           ),
-//         },
-//       }),
-//       Bareground: dataValue('Bare ground %'),
-//     };
-//     return upsert('WCSPROGRAMS_Vegetation', 'AnswerId', VegMap)(state);
-//   })
-// );
+  return upsertMany(
+    'WCSPROGRAMS_Vegetation',
+    'Answer_ID',
+    () => VegMap
+  )(state);
+});
 
-// THIS OPERATION REPLACES THE ONE BELOW IT.
-// It shows an optimal way of dealing with async/await on mssql
 alterState(async state => {
   const dataArray = state.survey['liana_old'];
   const BrushMap = [];
@@ -261,29 +156,6 @@ alterState(async state => {
     () => BrushMap
   )(state);
 });
-// =============================================================
-
-// each(
-//   'survey.liana_old[*]',
-//   alterState(async state => {
-//     const BrushMap = {
-//       Answer_Id: dataValue('surveyid'),
-//       WCSPROGRAMS_TaxaID: await findValue({
-//         uuid: 'WCSPROGRAMS_TaxaID',
-//         relation: 'WCSPROGRAMS_Taxa',
-//         where: {
-//           ScientificName: `%${state.handleValue(data[`Liana`])}%`,
-//         },
-//         operator: { ScientificName: 'like' },
-//       })(state),
-//       LianaPercentage: dataValue('Liana percentage'),
-//       SurveySite: dataValue('survey_area'),
-//       TransectNo: dataValue('Transect_no'),
-//       PlotNumber: dataValue('Plot_no'),
-//     };
-//     return upsert('WCSPROGRAMS_VegetationBrush', 'AnswerId', BrushMap)(state);
-//   })
-// );
 
 each(
   'survey.ground_species[*]',
