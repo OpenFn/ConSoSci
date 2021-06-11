d7d4009cbf77706f804c41f424ab597f6b9eabfb
Taylor Downs <taylor@openfn.org>
2021-06-11 17:37:30 +0100

    ddetoeuf@wcs.org's change on OpenFn.org

--- vegetation/legacyVegetation_vDD.js
+++ vegetation/legacyVegetation_vDD.js
@@ -20,6 +20,7 @@ alterState(async state => {
       Answer_ID: data['surveyid'],
       WCSPROGRAMS_VegetationName: 'LegacyData',
       SubmissionDate:'11/06/2021',
+      StartTime: '0',
       Surveydate: data['Date of survey'],
       SurveySite: data['Survey site'],
       TransectNo: data['Transect No'],
