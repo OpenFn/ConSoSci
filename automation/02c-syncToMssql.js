6c4ed0fd0c1612d722e83aa6f857e3dc806533ab
mamadou <mamadou@openfn.org>
2021-07-26 12:43:56 +0000

    redo foreach, #125

--- automation/02c-syncToMssql.js
+++ automation/02c-syncToMssql.js
@@ -36,7 +36,17 @@ each('$.forms[*]', state => {
 
             const columns = mergedColumns.filter(x => x.name !== undefined);
 
-            columns.forEach(convertToMssqlTypes);
+            columns.forEach(col =>
+              col.type === 'select_one' || col.type === 'select_multiple'
+                ? (col.type = 'text')
+                : col.type === 'int4' || col.type === 'float4'
+                ? (col.type = col.type.substring(0, col.type.length - 1))
+                : col.type === 'jsonb'
+                ? (col.type = 'nvarchar(max)')
+                : col.type === 'timestamp'
+                ? (col.type = 'datetime')
+                : col.type
+            );
             // Note: Specify options here (e.g {writeSql: false, execute: true})
             return insertTable(name, state => columns, {
               writeSql: true, // Keep to true to log query (otherwise make it false).
@@ -51,7 +61,17 @@ each('$.forms[*]', state => {
                 x.name !== undefined &&
                 !columnNames.includes(x.name.toLowerCase())
             );
-            newColumns.forEach(convertToMssqlTypes);
+            newColumns.forEach(col =>
+              col.type === 'select_one' || col.type === 'select_multiple'
+                ? (col.type = 'text')
+                : col.type === 'int4' || col.type === 'float4'
+                ? (col.type = col.type.substring(0, col.type.length - 1))
+                : col.type === 'jsonb'
+                ? (col.type = 'nvarchar(max)')
+                : col.type === 'timestamp'
+                ? (col.type = 'datetime')
+                : col.type
+            );
             console.log(newColumns);
             if (newColumns && newColumns.length > 0) {
               console.log('Existing table found in mssql --- Updating.');
