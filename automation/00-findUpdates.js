77fb53b8ffbf9db4e324a88396b3151103f2d5a3
Taylor Downs <taylor@openfn.org>
2021-07-29 17:05:14 +0100

    ddetoeuf@wcs.org's change on OpenFn.org

--- automation/00-findUpdates.js
+++ automation/00-findUpdates.js
@@ -6,12 +6,12 @@ get('https://kf.kobotoolbox.org/api/v2/assets/?format=json', {}, state => {
   // ===========================================================================
   // == FOR ADMINS: Update the below `manualFormList` to designate which Kobo forms to sync ==//
   const manualFormList = [
-    // {
-    //   uid: 'apZrpKcK78xzrPcAfRrfac',
-    //   p1: 'WCSPROGRAMS',
-    //   p2: '',
-    //   tableId: 'SharksRays',
-    // },
+     {
+       uid: 'apZrpKcK78xzrPcAfRrfac',
+       p1: 'WCSPROGRAMS',
+       p2: '',
+       tableId: 'SharksRays',
+     },
     //=== Trillion Trees forms =====
     // { 
     //   uid: 'aHPGTtrrLB4k3xDA9UZipu',
