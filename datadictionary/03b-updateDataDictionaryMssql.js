377b51f1bdd155e0a5af1dc5c936f9edf186d72b
mamadou <mamadou@openfn.org>
2021-09-09 12:08:48 +0000

    separate data update, #119

---
+++ datadictionary/03b-updateDataDictionaryMssql.js
@@ -0,0 +1,44 @@
+fn(state => ({ ...state, formDefinition: state.data.formDefinition }));
+
+fn(state => {
+  console.log(state.formDefinition);
+  return state;
+});
+
+upsert('KoboToolBox_Forms', 'form_id', {
+  form_name: state.formDefinition.name,
+  date_created: state.formDefinition.date_created,
+  date_modified: state.formDefinition.date_modified,
+  form_owner: state.formDefinition.owner__username,
+  languages: state.formDefinition.summary.languages.join(','),
+  form_id: state.formDefinition.uid,
+  form_group: state => state.prefix2,
+  table_id: state => `${state.prefix1}_${state.prefix2}_${state.tableId}`,
+});
+
+upsertMany('KoboToolBox_Choices', 'list_id', state => {
+  const { choices } = state.formDefinition.content;
+  const formId = state.formDefinition.uid;
+  return choices.map(x => ({
+    list_id: `${x.list_name}${formId}${x['$kuid']}`,
+    list_name: x.list_name,
+    choice_name: x.name,
+    choice_label: x.label ? x.label.join(',') : '',
+    form_uid: formId,
+  }));
+});
+
+upsertMany('KoboToolBox_Questions', 'question_id', state => {
+  const { survey } = state.formDefinition.content;
+  const formId = state.formDefinition.uid;
+  return survey.map(x => ({
+    question_id: `${x['$kuid']}-${formId}`,
+    form_id: formId,
+    analytics_label: '',
+    question_name: x.name,
+    label: x.label ? x.label.join(',') : '',
+    question_type: x.type,
+    select_from_list_name: x.select_from_list_name,
+    question_constraint: x.constraint,
+  }));
+});
