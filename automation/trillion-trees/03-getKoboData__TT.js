c06ecffe4935bb17ee9a9cb1dcaebda58370244d
aleksa-krolls <aleksagk@gmail.com>
2021-11-02 15:12:12 +0200

    add TT job copies

---
+++ automation/trillion-trees/03-getKoboData__TT.js
@@ -0,0 +1,83 @@
+//== Job to be used for fetching data from Kobo on repeated, timer basis  ==//
+// This can be run on-demand at any time by clicking "run" //
+
+alterState(state => {
+  console.log('Current cursor value:', state.lastEnd);
+
+  // IF YOU CLEAR STATE...
+  // Set this manual cursor to the earliest submission date you want fetch.
+  const manualCursor = '2017-05-01T14:32:43.325+01:00';
+  state.data = {
+    surveys: [
+      {
+        uid: 'aaknL3DQQgkgZ8iay89X5P',
+        formName: 'SHARC',
+        tableId: 'WCSPROGRAMS_SharksRays',
+      },
+      //==== Addtl SharksRays to sync data ===============//
+    {
+      uid: 'aStMvYShWXZsKYa7AyN6sr',
+      formName: 'Sharks_Rays_20210521_Kenya',
+      tableId: 'WCSPROGRAMS_SharksRays',
+    },
+    {
+      uid: 'aQeXAtEkgg8PGwxDiCUnPW',
+      formName: 'Sharks_Rays_20210627_Kenya',
+      tableId: 'WCSPROGRAMS_SharksRays',
+    },
+    {
+      uid: 'ayvuo4RnYJBvMLUdNzhYgQ',
+      formName: 'Sharks_Rays_20210701_Kenya',
+      tableId: 'WCSPROGRAMS_SharksRays',
+    }
+    //=========================//
+    ].map(survey => ({
+      ...survey,
+      formId: survey.uid,
+      url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.uid}/data/?format=json`,
+      query: `&query={"_submission_time":{"$gte":"${
+        state.lastEnd || manualCursor
+      }"}}`,
+    })),
+  };
+  return state;
+});
+
+each(dataPath('surveys[*]'), state => {
+  const { url, query, tag, formId, formType, formName, tableId, owner } = state.data;
+  return get(`${url}${query}`, {}, state => {
+    state.data.submissions = state.data.results.map(submission => {
+      return {
+        // Here we append the tags defined above to the Kobo form submission data
+        tableId,
+        formName,
+        formOwner: owner,
+        formType,
+        body: submission,
+      };
+    });
+    const count = state.data.submissions.length;
+    console.log(`Fetched ${count} submissions from ${formName} (${tableId}).`);
+    //Once we fetch the data, we want to post each individual Kobo survey
+    //back to the OpenFn inbox to run through the jobs =========================
+    return each(dataPath('submissions[*]'), state => {
+      console.log(`Posting 1 of ${count}...`);
+      return post(state.configuration.openfnInboxUrl, {
+        body: state => state.data,
+      })(state);
+    })(state);
+    // =========================================================================
+  })(state);
+});
+
+alterState(state => {
+  let lastEnd = state.references
+    .filter(item => item.body)
+    .map(s => s.body.end)
+    .sort((a, b) => new Date(b.date) - new Date(a.date))[0];
+
+  lastEnd = new Date(lastEnd) > new Date() ? lastEnd : new Date().toISOString();
+
+  console.log('New cursor value:', lastEnd);
+  return { ...state, data: {}, references: [], lastEnd };
+});
---
+++ automation/trillion-trees/04-getKoboDataHistorical__TT.js
@@ -0,0 +1,64 @@
+//== Job to be used for fetching historical data from Kobo in one-time migrations ==//
+// This can be run on-demand at any time by clicking "run" //
+
+alterState(state => {
+  // BEFORE RUNNING THIS JOB...
+  // Set this manual cursor to the earliest submission date you want fetch. 
+  // Set this to a very early date if you want to fetch ALL data. 
+  const manualCursor = '2017-05-01T14:32:43.325+01:00';
+  console.log('Fetching Kobo submissions starting at: ', manualCursor); 
+  state.data = {
+    surveys: [
+      //LIST FORMS HERE THAT YOU WANT TO SYNC HIST DATA
+      // {
+      //   uid: 'aaknL3DQQgkgZ8iay89X5P',
+      //   formName: 'SHARC',
+      //   tableId: 'WCSPROGRAMS_SharksRays',
+      // },
+    //=========================//
+    ].map(survey => ({
+      ...survey,
+      formId: survey.uid,
+      url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.uid}/data/?format=json`,
+      query: `&query={"_submission_time":{"$gte":"${manualCursor}"}}`,
+    })),
+  };
+  return state;
+});
+
+each(dataPath('surveys[*]'), state => {
+  const { url, query, tag, formId, formType, formName, tableId, owner } = state.data;
+  return get(`${url}${query}`, {}, state => {
+    state.data.submissions = state.data.results.map(submission => {
+      return {
+        // Here we append the tags defined above to the Kobo form submission data
+        tableId,
+        formName,
+        formOwner: owner,
+        formType,
+        body: submission,
+      };
+    });
+    const count = state.data.submissions.length;
+    console.log(`Fetched ${count} submissions from ${formName} (${tableId}).`);
+    //Once we fetch the data, we want to post each individual Kobo survey
+    //back to the OpenFn inbox to run through the jobs =========================
+    return each(dataPath('submissions[*]'), state => {
+      console.log(`Posting 1 of ${count}...`);
+      return post(state.configuration.openfnInboxUrl, {
+        body: state => state.data,
+      })(state);
+    })(state);
+    // =========================================================================
+  })(state);
+});
+
+alterState(state => {
+  let lastEnd = state.references
+    .filter(item => item.body)
+    .map(s => s.body.end)
+    .sort((a, b) => new Date(b.date) - new Date(a.date))[0];
+
+  lastEnd = new Date(lastEnd) > new Date() ? lastEnd : new Date().toISOString();
+  return { ...state, data: {}, references: [], lastEnd };
+});
