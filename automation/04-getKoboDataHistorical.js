8cbdf1632b86282aabc57f07628bd3cc9c2ab402
Aleksa Krolls <aleksagk@gmail.com>
2021-11-02 13:15:13 +0200

    Update 04-getKoboDataHistorical.js

--- automation/04-getKoboDataHistorical.js
+++ automation/04-getKoboDataHistorical.js
@@ -1,68 +1,64 @@
-//== Job to be used for fetching data from Kobo for historical, once-off migrations  ==//
+//== Job to be used for fetching historical data from Kobo in one-time migrations ==//
 // This can be run on-demand at any time by clicking "run" //
 
 alterState(state => {
-  console.log(`Started at: ${new Date().toISOString()}`);
-  state.submissions = [];
+  // BEFORE RUNNING THIS JOB...
+  // Set this manual cursor to the earliest submission date you want fetch. 
+  // Set this to a very early date if you want to fetch ALL data. 
+  const manualCursor = '2017-05-01T14:32:43.325+01:00';
+  console.log('Fetching Kobo submissions starting at: ', manualCursor); 
   state.data = {
     surveys: [
-      //** Specify new forms to fetch here **//
-      // TODO: Add tag options
-      //** Tag options:  **//
-      // Historical  forms to migrate:
-      //Make sure the tag matches the Trigger of the related Job
-     // { id: 'ar9wXnLW2sdaamGgJsUrjP', tag: 'bns_survey', name: 'BNS extended Uganda 2020', owner: 'wcs_uganda_carbon'}, // synced Nov16,2020
-      
-      //******* OPENFN TEST FORMS ******///
-      //{ id: 'aijj9FSpuCzMTRnMPGike8', tag: 'nrgt_historical', name: 'NRGT Makira 2017 - Test OpenFn', owner: 'bns' }, // TEST
-      
-      //****** Ongoing Forms to Rerun ****// 
-     
-     //******* WCS HISTORICAL FORMS ******///
-
+      //LIST FORMS HERE THAT YOU WANT TO SYNC HIST DATA
+      // {
+      //   uid: 'aaknL3DQQgkgZ8iay89X5P',
+      //   formName: 'SHARC',
+      //   tableId: 'WCSPROGRAMS_SharksRays',
+      // },
+    //=========================//
     ].map(survey => ({
-      formId: survey.id,
-      tag: survey.tag,
-      name: survey.name, 
-      owner: survey.owner,
-      url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.id}/data/?format=json`,
+      ...survey,
+      formId: survey.uid,
+      url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.uid}/data/?format=json`,
+      query: `&query={"_submission_time":{"$gte":"${manualCursor}"}}`,
     })),
   };
-  console.log(`Fetching data for ${state.data.surveys.length} surveys.`);
   return state;
 });
 
 each(dataPath('surveys[*]'), state => {
-  const { url, tag, formId, name, owner } = state.data;
-  return get(url, {}, state => {
-    state.data.submissions = state.data.results.map((submission, i) => {
+  const { url, query, tag, formId, formType, formName, tableId, owner } = state.data;
+  return get(`${url}${query}`, {}, state => {
+    state.data.submissions = state.data.results.map(submission => {
       return {
-        i,
         // Here we append the tags defined above to the Kobo form submission data
-        form: tag,
-        formName: name, 
+        tableId,
+        formName,
         formOwner: owner,
+        formType,
         body: submission,
       };
     });
     const count = state.data.submissions.length;
-    console.log(`Fetched ${count} submissions from ${formId} (${tag}).`);
+    console.log(`Fetched ${count} submissions from ${formName} (${tableId}).`);
     //Once we fetch the data, we want to post each individual Kobo survey
     //back to the OpenFn inbox to run through the jobs =========================
     return each(dataPath('submissions[*]'), state => {
-      console.log(`Posting ${state.data.i + 1} of ${count}...`);
-      return post(state.configuration.openfnInboxUrl, { body: state => state.data })(state);
+      console.log(`Posting 1 of ${count}...`);
+      return post(state.configuration.openfnInboxUrl, {
+        body: state => state.data,
+      })(state);
     })(state);
     // =========================================================================
   })(state);
 });
 
 alterState(state => {
-  console.log(`Finished at: ${new Date().toISOString()}`);
-  return {
-    data: {
-      message: "No cursor required. Job fetches all submission for given forms."
-    },
-    references: []
-  };
+  let lastEnd = state.references
+    .filter(item => item.body)
+    .map(s => s.body.end)
+    .sort((a, b) => new Date(b.date) - new Date(a.date))[0];
+
+  lastEnd = new Date(lastEnd) > new Date() ? lastEnd : new Date().toISOString();
+  return { ...state, data: {}, references: [], lastEnd };
 });
