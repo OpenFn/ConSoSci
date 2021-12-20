bbc3e57defbaca149b52a8bff8c6fa9235a84ef0
Aleksa Krolls <aleksagk@gmail.com>
2021-12-20 16:52:00 -0500

    aleksa@openfn.org's change on OpenFn.org

--- connTest.js
+++ connTest.js
@@ -24,7 +24,7 @@
 sql({
   query: `INSERT INTO WCSPROGRAMS_DataTool (WCSPROGRAMS_DataToolName, WCSPROGRAMS_DataToolExtCode, 
   UserID_CR, UserID_LM, IsForCollect)
-VALUES ('Audio video interview', 'audio_video_interview', '0', '0', '0');
+VALUES ('Audio video interview', 'audio_video_interview', '0', '0', '1');
 `,
 });
 
