aaad23a690385f0c54cf43db810cb327ad702a23
Aleksa Krolls <aleksagk@gmail.com>
2022-01-26 15:47:38 +0200

    joseph@openfn.org's change on OpenFn.org

--- asana/upsertTask.js
+++ asana/upsertTask.js
@@ -1,11 +1,12 @@
 upsertTask('1201382240883590', { 
-  externalId: state.data.GrievanceID, //Per the docs, I think here we put the Asana external Id field name (e.g., 'gid')
+  externalId: 'gid', //Per the docs, I think here we put the Asana external Id field name (e.g., 'gid')
   data: {
+    gid: '1201687476823315',
     name: 'Joseph test task2',
     projects: ['1201382240883590'],  //WCS project gid
     created_at: state.data.start,
     completed_at: state.data.end,
-    notes: 'reported by Aleksa in Kobo',
+    notes: state.data.ReportFormat,
   
     
   },
@@ -18,7 +19,7 @@ state => {
 
 );
 
-updateTask('1201687476823315', {   //my task_gid
+/*updateTask('1201687476823315', {   //my task_gid
   name: 'Joseph test task2',
   custom_fields: {
       '1200603908440348': state.data.GrievanceOrSuggestion,   //GrievanceOrSuggestion
@@ -33,4 +34,4 @@ state => {
   }
 
 
-);
+);*/
