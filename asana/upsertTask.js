616fcb2fad7416280d0cb7848da5077895ec2959
Aleksa Krolls <aleksagk@gmail.com>
2022-01-26 08:25:06 +0200

    aleksa@openfn.org's change on OpenFn.org

--- asana/upsertTask.js
+++ asana/upsertTask.js
@@ -1,13 +1,13 @@
-upsertTask('1201382240883590', { //e.g., dataValue('body._id') to use the Kobo id from state.data
+upsertTask('1201382240883590', { 
   externalId: state.data.GrievanceID, //Per the docs, I think here we put the Asana external Id field name (e.g., 'gid')
   data: {
-    //Then we need to map the value for whichever external field we identify of Float32Array
-    //gid: dataValue('body._id') //fyi, in kobo _id is unique to the form submission
     name: 'Joseph test task2',
     projects: ['1201382240883590'],  //WCS project gid
     created_at: state.data.start,
     completed_at: state.data.end,
     notes: state.data.ReporterFullName,
+  
+    
   },
 },
 
@@ -15,4 +15,5 @@ state => {
     console.log(JSON.stringify(state.data, null, 2 )); //log data
     return state;
   }
+
 );
