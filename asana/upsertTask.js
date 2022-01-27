00a5565f2ccca10336f018be9a2d3f8af82db0ec
Aleksa Krolls <aleksagk@gmail.com>
2022-01-27 17:00:33 +0200

    joseph@openfn.org's change on OpenFn.org

--- asana/upsertTask.js
+++ asana/upsertTask.js
@@ -100,16 +100,14 @@ upsertTask(
   {
     externalId: 'gid', // Asana external Id field name (e.g., 'gid')
     data: {
-      /*gid: '1201687476823315',*/
+      gid: dataValue('body._id'),
       name: state =>
         `${dataValue('formName')(state)} some string ${dataValue(
           'body.GrievanceID'
         )(state)}`,
       projects: ['1201382240883590'], //WCS project gid
       notes: state =>
-        `${dataValue('formName')(state)} some string ${dataValue(
-          'body.ReportFormat'
-        )(state)}`,
+        `${dataValue('formName')(state)} some string ${dataValue('body.ReportFormat')(state)}`,
       custom_fields: {
           '1201382335160247': dataValue('body.OneDriveFolder'),  //Mapped to Grievance OneDrive Folder in Asana
           '1201382335160251': dataValue('body.DateGrievanceEntered'),  //Mapped to Submission Date in Asana
