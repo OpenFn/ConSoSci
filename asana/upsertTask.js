1f7685ff23dbd6f0e85f5092ec310b8194c83710
Aleksa Krolls <aleksagk@gmail.com>
2022-01-27 10:56:27 +0200

    joseph@openfn.org's change on OpenFn.org

--- asana/upsertTask.js
+++ asana/upsertTask.js
@@ -102,7 +102,7 @@ upsertTask(
       projects: ['1201382240883590'], //WCS project gid
       /*created_at: dataValue('body.start'),
     completed_at: dataValue('body.end'),*/
-      notes: dataValue('body.ReporterFullName') + dataValue('body.ReporterContactInformation'),
+      notes: dataValue('body.ReporterFullName' + 'body.ReporterContactInformation),
       custom_fields: {
         /*'1200603908440348': dataValue('body.GrievanceOrSuggestion'), */ //GrievanceOrSuggestion
         // 1192836094355010: '1192836094355011', //Grievance Report Format
