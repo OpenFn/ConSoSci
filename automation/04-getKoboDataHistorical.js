6b32e406e01dbf301b3dc08a78827737a571a8e6
Taylor Downs <taylor@openfn.org>
2022-02-21 08:55:21 +0000

    ddetoeuf@wcs.org's change on OpenFn.org

--- automation/04-getKoboDataHistorical.js
+++ automation/04-getKoboDataHistorical.js
@@ -30,11 +30,11 @@ alterState(state => {
     //  formName: 'Sharks_Rays_20210701_Kenya',
     //  tableId: 'WCSPROGRAMS_SharksRays',
     //}, //Resynced on Feb16, 2022
-    {
-      uid: 'aCp2CojjXBoazzhweWrRUr',
-      formName: 'Sharks_Rays_20210920_Mada',
-      tableId: 'WCSPROGRAMS_SharksRays',
-    }, //Resynced on Feb21, 2022
+    //{
+    //  uid: 'aCp2CojjXBoazzhweWrRUr',
+    //  formName: 'Sharks_Rays_20210920_Mada',
+    //  tableId: 'WCSPROGRAMS_SharksRays',
+    //}, //Resynced on Feb21, 2022
     {
       uid: 'aSDqbJNRcEkvgKHJSMK2z7',
       formName: 'Sharks_Rays_20220111_Mada',
