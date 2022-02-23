c61ee80c313db45fbab8f630622dec9b1dcb3313
Taylor Downs <taylor@openfn.org>
2022-02-23 11:05:57 +0000

    ddetoeuf@wcs.org's change on OpenFn.org

--- bns/survey.js
+++ bns/survey.js
@@ -280,7 +280,8 @@ upsert('WCSPROGRAMS_KoboData', 'DatasetUuidId', {
     if (formName === 'BNS Cross River 2017-2020') {
       return state.data.body.today.split('-')[0];
     }
-    console.log(new Date());
+    const year = Number(date.trim().split('-')[0]);
+    console.log(year);
     return new Date().getFullYear();
     //console.log(Date().getFullYear());
   },
