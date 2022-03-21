03a7228f7d885be61ff64a5cc3422b9ad28c5558
Taylor Downs <taylor@openfn.org>
2022-03-21 15:59:52 +0000

    mvazquez@disgrafic.com's change on OpenFn.org

--- ngrt/2017_v2.js
+++ ngrt/2017_v2.js
@@ -1,2 +1,90 @@
-// Your job goes here.
-//
+fn(state => {
+  try {
+    const { body, formName, instance } = state.data;
+    const { _submission_time, _id, _xform_id_string } = body;
+    let cleanedSubmission = {};
+
+    for (const key in body) {
+      switch (body[key]) {
+        case 'yes':
+          cleanedSubmission[key] = 1;
+          break;
+
+        case 'no':
+          cleanedSubmission[key] = 0;
+          break;
+
+        default:
+          cleanedSubmission[key] = body[key];
+          break;
+      }
+    }
+
+    state.landscapeMap = {
+      tns: 'ndoki',
+      mamabay: 'makira',
+      mtkb: 'kahuzi',
+      lactele: 'lac_tele',
+    };
+
+    cleanedSubmission.durableUUID = `${_submission_time}-${_xform_id_string}-${_id}`;
+    cleanedSubmission.datasetId = `${formName}-${_xform_id_string}`;
+    cleanedSubmission.instance = instance;
+    state.data = cleanedSubmission;
+    return state;
+  } catch (error) {
+    state.connection.close();
+    throw error;
+  }
+});
+
+upsert('WCSPROGRAMS_KoboNrgtNrgtanswer', 'AnswerId', {
+  DatasetUuidId: dataValue('datasetId'),
+  AnswerId: dataValue('_id'),
+  Landscape: state => {
+    var landscape = dataValue('landscape')(state);
+    return state.landscapeMap[landscape] || landscape;
+  },
+  GovGroup: dataValue('gov_group'),
+  Jurisdiction: dataValue('jurisdiction'),
+  Objective: dataValue('objective'),
+  Members: dataValue('members'),
+  Women: dataValue('women'),
+  LastUpdate: new Date().toISOString(),
+});
+
+upsert('WCSPROGRAMS_KoboNrgtNrgtanswergs', 'AnswerId', {
+  DatasetUuidId: dataValue('datasetId'),
+  Id: dataValue('_id'),
+  AnswerId: dataValue('_id'),
+  SurveyDate: dataValue('today'),
+  Code: dataValue('code'),
+  Gender: dataValue('gender'),
+  Member: dataValue('member'),
+  Legitimacy: dataValue('legitimacy'),
+  Accountability: dataValue('accountability'),
+  Transparency: dataValue('transparency'),
+  Participation: dataValue('participation'),
+  Fairness: dataValue('fairness'),
+  KnowledgeSkills: dataValue('knowledge_skills'),
+  Resources: dataValue('resources'),
+  InstutionalFramework: dataValue('institutional_framework'),
+  Motivation: dataValue('motivation'),
+  EnactDecision: dataValue('enact_decision'),
+  HeldAccountable: dataValue('held_accountable'),
+  Diversity: dataValue('diversity'),
+  LastUpdate: new Date().toISOString(),
+});
+
+upsert('WCSPROGRAMS_KoboData', 'DatasetUuidId', {
+  DatasetName: state.data.formName,
+  DatasetOwner: state.data.formOwner,
+  DatasetUuidId: dataValue('datasetId'),
+  Citation: dataValue('instance'),
+  DatasetYear: new Date().getFullYear(),
+  LastSubmissionTime: dataValue('_submission_time'),
+  LastCheckedTime: dataValue('_submission_time'),
+  LastUpdateTime: new Date().toISOString(),
+  KoboManaged: true,
+  Tags: dataValue('_tags'),
+});
