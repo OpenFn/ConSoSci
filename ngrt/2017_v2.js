b00027d9a290b1ebc39c3f4990844a6ef2eac085
Taylor Downs <taylor@openfn.org>
2022-03-21 16:43:39 +0000

    mvazquez@disgrafic.com's change on OpenFn.org

--- ngrt/2017_v2.js
+++ ngrt/2017_v2.js
@@ -38,7 +38,7 @@ fn(state => {
   }
 });
 
-/*
+
 upsert('WCSPROGRAMS_KoboNrgtNrgtanswer', 'AnswerId', {
   DatasetUuidId: dataValue('datasetId'),
   AnswerId: dataValue('_id'),
@@ -89,9 +89,9 @@ upsert('WCSPROGRAMS_KoboData', 'DatasetUuidId', {
   KoboManaged: true,
   Tags: dataValue('_tags'),
 });
-*/
 
-console.log("hello?");
+
+console.log("hello");
 
 // test
 each(
