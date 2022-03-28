27b31a90062f6829313af574dea3447a393a6ffc
Taylor Downs <taylor@openfn.org>
2022-03-28 12:15:55 +0100

    ddetoeuf@wcs.org's change on OpenFn.org

--- automation/00-findUpdates.js
+++ automation/00-findUpdates.js
@@ -14,12 +14,12 @@ get('https://kf.kobotoolbox.org/api/v2/assets/?format=json', {}, state => {
       p2: '',
       tableId: 'SharksDecTest',
     },*/
-    {
-        uid: 'aDgPJqN4SAYohZ4ZueEeYU',
-        p1: 'WCSPROGRAMS',
-        p2: 'Arcadia',
-        tableId: 'SiteSurvey'
-    },
+    //{
+    //    uid: 'aDgPJqN4SAYohZ4ZueEeYU',
+    //    p1: 'WCSPROGRAMS',
+    //    p2: 'Arcadia',
+    //    tableId: 'SiteSurvey'
+    //},
     // Trillion Trees Testing //NOTE: openfn_kobo does not have access to this form
       //     {
       // uid: 'a587nJHzyBYATXi3Nb2yc7',
@@ -28,12 +28,12 @@ get('https://kf.kobotoolbox.org/api/v2/assets/?format=json', {}, state => {
       // tableId: 'SiteRegistration'
       // },
     //=== WCS Socio Economic Database =====
-    // {
-    //     uid: 'aukhdejQU76K33caCkF4rP',
-    //     p1: 'WCSPROGRAMS',
-    //     p2: 'SocioEco',
-    //     tableId: 'SocioEcoSurvey'
-    // },
+     {
+         uid: 'aukhdejQU76K33caCkF4rP',
+         p1: 'WCSPROGRAMS',
+         p2: 'SocioEco',
+         tableId: 'SocioEcoSurvey'
+     },
     //==== SharksRays ===============//
     // {
     //   uid: 'aaknL3DQQgkgZ8iay89X5P',
