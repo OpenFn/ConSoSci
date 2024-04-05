af99bbca104644213ee2588a6c1950034543083c
Hunter Achieng <achienghunter@gmail.com>
2024-04-05 19:35:02 +0300

    feat: implement upsert asana task
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- form-sharing/4-upsertAsanaTask.js
+++ form-sharing/4-upsertAsanaTask.js
@@ -1,16 +1,14 @@
-
-
-fn(state => {
-    console.log(state.filteredKoboFormsData);
-    return state
-})
-
-upsertTask("1198901998266253", {
-    externalId: "name",
-    data: {
-      name: "test",
-      approval_status: "pending",
-      projects: ["1203181218738601"],
-      assignee: "1203181218738601",
-    },
-  });
+each(
+  '$.filteredKoboFormData[*]',
+  upsertTask('1198901998266253', state => {
+    return {
+      externalId: 'name',
+      data: {
+        name: state.filteredKoboFormData.name,
+        approval_status: 'pending',
+        projects: ['1203181218738601'],
+        assignee: '1203181218738601',
+      },
+    };
+  })
+);
