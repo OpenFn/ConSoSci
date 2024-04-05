56b9d485eb04b3467a08a8544a130a87cde9ae14
Hunter Achieng <achienghunter@gmail.com>
2024-04-05 13:47:14 +0300

    feat: implement reading and filtering uids from googlesheets
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- form-sharing/2-getSheetsList.js
+++ form-sharing/2-getSheetsList.js
@@ -1,3 +1,14 @@
+getValues(
+  '1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY',
+  'sheetsList of kobo forms!A:K'
+);
 fn(state => {
-    console.log(state);
-})
+  const koboFormsData = state.references[1];
+  const googleSheetsData = state.data.values.map(row => row[0]);
+
+  state.data = koboFormsData.filter(
+    form => !googleSheetsData.includes(form.uid)
+  );
+
+  return state;
+});
