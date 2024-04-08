845d5ed26a1683242ba0ee70b8dd8431dc6c6e28
Hunter Achieng <achienghunter@gmail.com>
2024-04-08 07:50:53 +0300

    fix: upsert to asana task
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- form-sharing/4-upsertAsanaTask.js
+++ form-sharing/4-upsertAsanaTask.js
@@ -1,14 +1,28 @@
-each(
-  '$.filteredKoboFormData[*]',
-  upsertTask('1198901998266253', state => {
+fn(state => {
+  const today = new Date();
+
+  const futureDate = new Date(today.getTime() + 5 * 24 * 60 * 60 * 1000);
+
+  const dueDate = futureDate.toISOString().split('T')[0];
+
+  state.asanaTasks = state.filteredKoboFormsData.map(form => {
     return {
-      externalId: 'name',
-      data: {
-        name: state.filteredKoboFormData.name,
-        approval_status: 'pending',
-        projects: ['1203181218738601'],
-        assignee: '1203181218738601',
-      },
+      name: `New form added to OpenFn: ${form.name}`,
+      approval_status: 'pending',
+      projects: ['1198901998266253'],
+      assignee_section: '1203181218738601',
+      assignee: '473999120764595',
+      due_on: dueDate,
+      notes: `New form added to OpenFn: ${form.name}. Please review the Google Sheet and add the manual values missing: https://docs.google.com/spreadsheets/d/1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY/edit#gid=0`,
     };
+  });
+
+  return state;
+});
+each(
+  '$.asanaTasks[*]',
+  upsertTask('1198901998266253', {
+    externalId: 'name',
+    data: state => state.data,
   })
 );
