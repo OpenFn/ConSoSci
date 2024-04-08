36f48ee4d96292baa75193ecee443cacd30704eb
Hunter Achieng <achienghunter@gmail.com>
2024-04-08 14:01:10 +0300

    fix: use callbacks in getValues
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- form-sharing/2-getSheetsList.js
+++ form-sharing/2-getSheetsList.js
@@ -1,14 +1,16 @@
 getValues(
   '1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY',
-  'sheetsList of kobo forms!A:K'
+  'sheetsList of kobo forms!A:K',
+  state => {
+    const { koboForms, data } = state;
+    const [headers, ...sheetsData] = data.values;
+    const sheetsUids = sheetsData.map(row => row[0]);
+    console.log('Ignoring headers', headers);
+
+    state.filteredKoboFormsData = koboForms.filter(
+      form => !sheetsUids.includes(form.uid)
+    );
+
+    return state;
+  }
 );
-fn(state => {
-  const { koboForms} = state;
-  const googleSheetsData = state.data.values.map(row => row[0]);
-
-  state.filteredKoboFormsData = koboForms.filter(
-    form => !googleSheetsData.includes(form.uid)
-  );
-
-  return state;
-});
