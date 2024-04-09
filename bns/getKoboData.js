aa41c286bf836d1a067e2fc1e2e38c7f829e5421
Hunter Achieng <achienghunter@gmail.com>
2024-04-09 12:49:09 +0300

    feat: implement get google sheets job
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- bns/getKoboData.js
+++ bns/getKoboData.js
@@ -1,48 +1,45 @@
+import { http } from '@openfn/language-googlesheets';
 //== Job to be used for fetching data from Kobo on repeated, timer basis  ==//
 // This can be run on-demand at any time by clicking "run" //
-getValues('1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY', 'sheetsList of Kobo forms!A:L', state => {
-  console.log(state);
-})
+getValues(
+  '1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY',
+  'sheetsList of Kobo forms!A:L',
+  state => {
+    const [headers, ...values] = state.data.values;
+
+    const filteredSheetsData = values.filter(item => {
+      if (
+        item.includes('TRUE') &&
+        item.includes('bns_survey', 'nrgt_current')
+      ) {
+        return item;
+      }
+    });
+
+    state.sheetsData = filteredSheetsData.map(item => {
+      return headers.reduce((obj, header) => {
+        obj[header] = item[headers.indexOf(header)];
+        return obj;
+      }, {});
+    });
+
+    return state;
+  }
+);
 
 alterState(state => {
+  const { sheetsData } = state;
+
   console.log('Current cursor value:', state.lastEnd);
   // Set a manual cursor if you'd like to only fetch data after this date.
   const manualCursor = '2023-01-01T23:51:45.491+01:00';
   state.data = {
-    surveys: [
-      //** Specify new forms to fetch here **//
-      //** Tag options: bns_survey, bns_price, nrgt_current, nrgt_historical  **//
-      //{ id: 'aMpW7wwRBRbtCfidtK2rRn', tag: 'bns_survey', name: 'Form Project Name', owner: 'wcs'},
-
-// ONGOING BNS household forms
-  { id: 'aGfcUnks7JLms4YCGbHgwX', tag: 'bns_survey', name: 'BNS intervention Cross River 2024', owner: 'cemogor', instance: 'C. Emogor, 2023' },// 
-  { id: 'aJrXKdPgXfwSnam5iwaRHq', tag: 'bns_survey', name: 'BNS Ciblage 4e cohorte 2024', owner: 'wcs_poultry', instance: 'A. M. Boucka, A. Nkounkou, WCS Congo unpublished data 2024' },// New landscape: NdokiPeriphery
-  { id: 'a5jucWV84nFeP3BeHtbRMU', tag: 'bns_survey', name: 'BNS Menage PNMD 2023', owner: 'wcs_paapnmd', instance: 'WCS Cameroon unpublished data 2023' }, 
-  { id: 'aK3MS8ATY53KXsKCm7xzpY', tag: 'bns_survey', name: 'BNS Ituri : Enquête Ménages 2023', owner: 'wcs_ituri', instance: 'WCS DRC unpublished data 2023' }, 
-  { id: 'aJVZmRRfj442Li5F6r6M8y', tag: 'bns_survey', name: 'BNS ménage Kahuzi 2023', owner: 'wcs_mtkb', instance: 'WCS DRC unpublished data 2023'}, 
-  { id: 'a5s3aXxviNV3GhMANSSaGk', tag: 'bns_survey', name: 'BNS ménage Kabobo 2023', owner: 'wcs_pcbk', instance: 'WCS DRC unpublished data 2023'}, 
-  { id: 'aAGC9q7nwXPnVLP6bFNAEw', tag: 'bns_survey', name: 'BNS_Individual_Niassa_2023', owner: 'wcs_niassa', instance: 'WCS Niassa unpublished data 2023'}, 
-
-// ONGOING BNS prices forms
-  { id: 'aKVvMRrvTTCc8j7d3EDh7a', tag: 'bns_price', name: 'BNS Prix PNMD 2023',  owner: 'wcs_paapnmd', instance: 'WCS Cameroon unpublished data 2023' }, 
-  { id: 'a4oeehbiGuXrEWK7rkUv82', tag: 'bns_price', name: 'BNS Prix Ituri 2023',  owner: 'wcs_ituri', instance: 'WCS DRC unpublished data 2023'  }, 
-  { id: 'a3kbAt2freW3q8Ht48V3q2', tag: 'bns_price', name: 'BNS Prix Kahuzi 2023', owner: 'wcs_mtkb', instance: 'WCS DRC unpublished data 2023' }, 
-  { id: 'aH2dvuU2G8wiVZQ3k4fiRE', tag: 'bns_price', name: 'BNS Prix Kabobo 2023', owner: 'wcs_pcbk', instance: 'WCS DRC unpublished data 2023'}, 
-  { id: 'aAKdquWgPSLjzB3UgGBcsW', tag: 'bns_price', name: 'BNS_Precos_Niassa_2023', owner: 'wcs_niassa', instance: 'WCS Niassa unpublished data 2023'},  
-      
-// ONGOING NRGT forms
-  { id: 'aKYgSnUiLd8KkevPV2ty6e', tag: 'nrgt_current', name: 'NRGT NW Janvier 2024', owner: 'bemahafaly_wcs', instance: 'WCS Madagascar unpublished data 2024'}, 
-  { id: 'aDEEXL9fXQhZXpdaKPnrcJ', tag: 'nrgt_current', name: 'NRGT_Makira_2023_revisé', owner: 'wcs_mamabaie', instance: 'WCS Madagascar unpublished data 2023'}, 
-  { id: 'a33XvMuPQLpeygLURuNUBP', tag: 'nrgt_current', name: 'NRGT SWM 2023', owner: 'wcs_ndoki', instance: 'WCS Congo unpublished data 2023' }, 
-  { id: 'aqDKwe8AD3ykFeNEDuLSnv', tag: 'nrgt_current', name: 'Nosy Be NRGT 2023', owner: 'wcs_library', instance: 'WCS Madagascar unpublished data 2023' }, 
-  { id: 'a8KBiBL44hEpNfkS4RmxeN', tag: 'nrgt_current', name: 'NRGT_Niassa_2023', owner: 'wcs_niassa', instance: 'WCS Niassa unpublished data 2023'}, 
-
-    ].map(survey => ({
-      formId: survey.id,
+    surveys: sheetsData.map(survey => ({
+      formId: survey.uid,
       tag: survey.tag,
       name: survey.name,
       owner: survey.owner,
-      url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.id}/data/?format=json`,
+      url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.uid}/data/?format=json`,
       query: `&query={"end":{"$gte":"${state.lastEnd || manualCursor}"}}`,
     })),
   };
@@ -51,7 +48,7 @@ alterState(state => {
 
 each(dataPath('surveys[*]'), state => {
   const { url, query, tag, formId, name, owner } = state.data;
-  return get(`${url}${query}`, {}, state => {
+  return http.get(`${url}${query}`, {}, state => {
     state.data.submissions = state.data.results.map((submission, i) => {
       return {
         i,
@@ -68,7 +65,7 @@ each(dataPath('surveys[*]'), state => {
     //back to the OpenFn inbox to run through the jobs =========================
     return each(dataPath('submissions[*]'), state => {
       console.log(`Posting ${state.data.i + 1} of ${count}...`);
-      return post(state.configuration.openfnInboxUrl, {
+      return http.post(state.configuration.openfnInboxUrl, {
         body: state => state.data,
       })(state);
     })(state);
