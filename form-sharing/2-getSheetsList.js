a30070fa6f91ef2d5c8f7ba3f7a859373f137d55
Aleksa Krolls <aleksagk@gmail.com>
2024-04-21 19:17:39 +0300

    aleksa@openfn.org's change on OpenFn.org

--- form-sharing/2-getSheetsList.js
+++ form-sharing/2-getSheetsList.js
@@ -1,5 +1,6 @@
+//Get list of forms we definitely want to sync to the DB; compare with new forms in Kobo 
 getValues(
-  '1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY',
+  '1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY', //googlesheet id 
   'sheetsList of kobo forms!A:K',
   state => {
     const { koboForms, data } = state;
