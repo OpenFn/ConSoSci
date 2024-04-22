41c58f3c59b3d4b0b40816c733f1b55ff9786b0e
Aleksa Krolls <aleksagk@gmail.com>
2024-04-22 15:34:37 +0300

    mtuchi@openfn.org's change on OpenFn.org

--- form-sharing/2-getSheetsList.js
+++ form-sharing/2-getSheetsList.js
@@ -1,18 +1,17 @@
 //Get list of forms we definitely want to sync to the DB; compare with new forms in Kobo 
 getValues(
-  '1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY', //googlesheet id 
-  'sheetsList of kobo forms!A:K'
-);
-
-fn(state => {
+  "1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY", //googlesheet id
+  "sheetsList of kobo forms!A:K",
+  (state) => {
     const { koboForms, data } = state;
     const [headers, ...sheetsData] = data.values;
-    const sheetsUids = sheetsData.map(row => row[0]);
-    console.log('Ignoring headers', headers);
+    const sheetsUids = sheetsData.map((row) => row[0]);
+    console.log("Ignoring headers", headers);
 
     state.filteredKoboFormsData = koboForms.filter(
-      form => !sheetsUids.includes(form.uid)
+      (form) => !sheetsUids.includes(form.uid)
     );
 
     return state;
-  })
+  }
+);
