1071730c28935c16a77033a812b16dcbd5199e0e
Aleksa Krolls <aleksagk@gmail.com>
2024-04-23 08:32:35 +0300

    aleksa@openfn.org's change on OpenFn.org

--- form-sharing/2-getSheetsList.js
+++ form-sharing/2-getSheetsList.js
@@ -1,6 +1,6 @@
 getValues(
   "1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY", //googlesheet id
-  "sheetsList of kobo forms!A:K"
+  "sheetsList of kobo forms!A:L" //range of columns in sheet
 );
 
 fn((state) => {
