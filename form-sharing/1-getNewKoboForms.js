a806cc15dfbc71c0211254f158273c38efd88bd3
Aleksa Krolls <aleksagk@gmail.com>
2024-05-06 23:24:01 +0300

    aleksa@openfn.org's change on OpenFn.org

--- form-sharing/1-getNewKoboForms.js
+++ form-sharing/1-getNewKoboForms.js
@@ -1,6 +1,10 @@
 //Check Kobo account for forms with these matching keywords
 getForms({}, state => {
-  const keywords = ['price', 'prix', 'bns', 'nrgt', 'grm', 'feedback'];
+  //ALL KEYWORDS: 
+  //const keywords = ['price', 'prix', 'bns', 'nrgt', 'grm', 'feedback'];
+  
+  //BNS KEYWORDS ONLY
+  const keywords = ['price', 'prix', 'bns', 'nrgt'];
 
   const checkForKeyWords = name => {
     return keywords.some(keyword => name.toLowerCase().includes(keyword));
