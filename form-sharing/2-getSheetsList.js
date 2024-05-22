eab3c99642e4431eac437754fc809c16e66a0300
Emmanuel Evance <mtuchidev@gmail.com>
2024-05-22 16:20:48 +0300

    update sheet status and date_modified column

--- form-sharing/2-getSheetsList.js
+++ form-sharing/2-getSheetsList.js
@@ -1,18 +1,35 @@
 getValues(
-  "1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY", //googlesheet id
-  "wcs-bns-test!A:L" //range of columns in sheet
+  '1s7K3kxzm5AlpwiALattyc7D9_aIyqWmo2ubcQIUlqlY', //googlesheet id
+  'mtuchi-testing!A:N' //range of columns in sheet
 );
 
-fn((state) => {
-  const { koboForms, data } = state;
+fn(state => {
+  const { activeForms, archivedForms, data } = state;
   const [headers, ...sheetsData] = data.values;
-  const sheetsUids = sheetsData.map((row) => row[0]);
-  console.log("Ignoring headers", headers);
+  const sheetsUids = sheetsData.map(row => row[0]);
+  console.log('Ignoring headers', headers);
 
-  state.filteredKoboFormsData = koboForms.filter(
-    (form) => !sheetsUids.includes(form.uid)
+  state.formsToCreate = activeForms.filter(
+    form => !sheetsUids.includes(form.uid)
   );
-  state.data = {};
-  state.references = [];
+
+  state.formsToUpdate = archivedForms
+    .filter(form => sheetsUids.includes(form.uid))
+    .map(form => {
+      const rowIndex = sheetsData.findIndex(row => {
+        return row[0] === form.uid;
+      });
+      if (rowIndex !== -1) {
+        return { ...form, rowIndex };
+      }
+      console.log(form.uid, 'Could not be found in google sheet');
+    });
+
   return state;
+});
+
+fn(state => {
+  const { data, references, response, ...remainingState } = state;
+
+  return remainingState;
 });
