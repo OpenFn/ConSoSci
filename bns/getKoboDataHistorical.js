b636679e597257ecd4517d550b223d02606aad60
aleksa-krolls <aleksagk@gmail.com>
2024-08-24 12:30:20 +0300

    add jobs for historical sync

---
+++ bns/getKoboDataHistorical.js
@@ -0,0 +1,29 @@
+// Here we fetch submissions for all "Archived" forms in GoogleSheet
+// NOTE: See linked job "[BNS-1B] 1.Get FormsList (Historical)" for GoogleSheet query logic
+//**********************************************************//
+each(dataPath('surveys[*]'), state => {
+  const { url, query, tag, formId, name, owner } = state.data;
+  return get(`${url}${query}`, {}, state => {
+    state.data.submissions = state.data.results.map((submission, i) => {
+      return {
+        i,
+        // Here we append the tags defined above to the Kobo form submission data
+        form: tag,
+        formName: name,
+        formOwner: owner,
+        body: submission,
+      };
+    });
+    const count = state.data.submissions.length;
+    console.log('Finding historical forms to resync...');
+    console.log(`Fetched ${count} submissions from ${formId} (${tag}).`);
+    //Once we fetch the data, we want to post each individual Kobo survey
+    //back to the OpenFn inbox to run through the jobs =========================
+    return each(dataPath('submissions[*]'), state => {
+      console.log(`Posting ${state.data.i + 1} of ${count}...`);
+      return post(state.configuration.openfnInboxUrl, {
+        body: state => state.data,
+      })(state);
+    })(state);
+  })(state);
+});
